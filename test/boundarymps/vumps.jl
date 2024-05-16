b5affa60ae43a68a147244f480422a128b0e7da6
leburgel <lander.burgelman@gmail.com>
2024-05-16 14:51:48 +0200

    Wrap boundary mps tests in testsets

--- test/boundarymps/vumps.jl
+++ test/boundarymps/vumps.jl
@@ -7,61 +7,66 @@ using LinearAlgebra
 
 Random.seed!(29384293742893)
 
-# (1, 1) unit cell
+@testset "(1, 1) PEPS" begin
+    psi = InfinitePEPS(ComplexSpace(2), ComplexSpace(2))
 
-psi = InfinitePEPS(ComplexSpace(2), ComplexSpace(2))
+    T = PEPSKit.InfiniteTransferPEPS(psi, 1, 1)
+    mps = PEPSKit.initializeMPS(T, [ComplexSpace(20)])
 
-T = PEPSKit.InfiniteTransferPEPS(psi, 1, 1)
-mps = PEPSKit.initializeMPS(T, [ComplexSpace(20)])
+    mps, envs, ϵ = leading_boundary(mps, T, VUMPS())
+    N = abs(sum(expectation_value(mps, T)))
 
-mps, envs, ϵ = leading_boundary(mps, T, VUMPS())
-N = abs(sum(expectation_value(mps, T)))
+    ctm = leading_boundary(
+        psi, CTMRG(; verbosity=1, fixedspace=true), CTMRGEnv(psi; Venv=ComplexSpace(20))
+    )
+    N´ = abs(norm(psi, ctm))
 
-ctm = leading_boundary(
-    psi, CTMRG(; verbosity=1, fixedspace=true), CTMRGEnv(psi; Venv=ComplexSpace(20))
-)
-N´ = abs(norm(psi, ctm))
-
-@test N ≈ N´ atol = 1e-3
-
-# (2, 2) unit cell
-
-psi2 = InfinitePEPS(ComplexSpace(2), ComplexSpace(2); unitcell=(2, 2))
-T2 = PEPSKit.TransferPEPSMultiline(psi2, 1)
-
-mps2 = PEPSKit.initializeMPS(T2, fill(ComplexSpace(20), 2, 2))
-mps2, envs2, ϵ = leading_boundary(mps2, T2, VUMPS())
-N2 = abs(prod(expectation_value(mps2, T2)))
-
-ctm2 = leading_boundary(
-    psi2, CTMRG(; verbosity=1, fixedspace=true), CTMRGEnv(psi2; Venv=ComplexSpace(20))
-)
-N2´ = abs(norm(psi2, ctm2))
-
-@test N ≈ N´ rtol = 1e-3
-
-# PEPO contraction run through
-
-function ising_pepo(beta; unitcell=(1, 1, 1))
-    t = ComplexF64[exp(beta) exp(-beta); exp(-beta) exp(beta)]
-    q = sqrt(t)
-
-    O = zeros(2, 2, 2, 2, 2, 2)
-    O[1, 1, 1, 1, 1, 1] = 1
-    O[2, 2, 2, 2, 2, 2] = 1
-    @tensor o[-1 -2; -3 -4 -5 -6] :=
-        O[1 2; 3 4 5 6] * q[-1; 1] * q[-2; 2] * q[-3; 3] * q[-4; 4] * q[-5; 5] * q[-6; 6]
-
-    O = TensorMap(o, ℂ^2 ⊗ (ℂ^2)' ← ℂ^2 ⊗ ℂ^2 ⊗ (ℂ^2)' ⊗ (ℂ^2)')
-
-    return InfinitePEPO(O; unitcell)
+    @test N ≈ N´ atol = 1e-3
 end
 
-O = ising_pepo(1)
-T3 = InfiniteTransferPEPO(psi, O, 1, 1)
+@testset "(2, 2) PEPS" begin
+    psi = InfinitePEPS(ComplexSpace(2), ComplexSpace(2); unitcell=(2, 2))
+    T = PEPSKit.TransferPEPSMultiline(psi, 1)
 
-mps3 = PEPSKit.initializeMPS(T3, [ComplexSpace(10)])
-mps3, envs3, ϵ = leading_boundary(mps3, T3, VUMPS())
-f = abs(prod(expectation_value(mps3, T3)))
+    mps = PEPSKit.initializeMPS(T, fill(ComplexSpace(20), 2, 2))
+    mps, envs, ϵ = leading_boundary(mps, T, VUMPS())
+    N = abs(prod(expectation_value(mps, T)))
 
-nothing
+    ctm = leading_boundary(
+        psi, CTMRG(; verbosity=1, fixedspace=true), CTMRGEnv(psi; Venv=ComplexSpace(20))
+    )
+    N´ = abs(norm(psi, ctm))
+
+    @test N ≈ N´ rtol = 1e-3
+end
+
+@testset "PEPO runthrough" begin
+    function ising_pepo(beta; unitcell=(1, 1, 1))
+        t = ComplexF64[exp(beta) exp(-beta); exp(-beta) exp(beta)]
+        q = sqrt(t)
+
+        O = zeros(2, 2, 2, 2, 2, 2)
+        O[1, 1, 1, 1, 1, 1] = 1
+        O[2, 2, 2, 2, 2, 2] = 1
+        @tensor o[-1 -2; -3 -4 -5 -6] :=
+            O[1 2; 3 4 5 6] *
+            q[-1; 1] *
+            q[-2; 2] *
+            q[-3; 3] *
+            q[-4; 4] *
+            q[-5; 5] *
+            q[-6; 6]
+
+        O = TensorMap(o, ℂ^2 ⊗ (ℂ^2)' ← ℂ^2 ⊗ ℂ^2 ⊗ (ℂ^2)' ⊗ (ℂ^2)')
+
+        return InfinitePEPO(O; unitcell)
+    end
+
+    psi = InfinitePEPS(ComplexSpace(2), ComplexSpace(2))
+    O = ising_pepo(1)
+    T = InfiniteTransferPEPO(psi, O, 1, 1)
+
+    mps = PEPSKit.initializeMPS(T, [ComplexSpace(10)])
+    mps, envs, ϵ = leading_boundary(mps, T, VUMPS())
+    f = abs(prod(expectation_value(mps, T)))
+end
