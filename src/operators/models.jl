fc3f3e043f158b4f8e98f3568b1217ae5214fd05
lkdvos <lukas.devos@ugent.be>
2024-07-02 16:20:30 +0200

    Update models to new interface

--- src/operators/models.jl
+++ src/operators/models.jl
@@ -11,11 +11,12 @@ function square_lattice_heisenberg(
     ::Type{T}=ComplexF64; Jx=-1, Jy=1, Jz=-1
 ) where {T<:Number}
     physical_space = ComplexSpace(2)
+    lattice = fill(physical_space, 1, 1)
     σx = TensorMap(T[0 1; 1 0], physical_space, physical_space)
     σy = TensorMap(T[0 im; -im 0], physical_space, physical_space)
     σz = TensorMap(T[1 0; 0 -1], physical_space, physical_space)
     H = (Jx * σx ⊗ σx) + (Jy * σy ⊗ σy) + (Jz * σz ⊗ σz)
-    return NLocalOperator{NearestNeighbor}(H / 4)
+    return nearest_neighbour_hamiltonian(lattice, H)
 end
 
 """
@@ -27,6 +28,8 @@ function square_lattice_pwave(
     ::Type{T}=ComplexF64; t::Number=1, μ::Number=2, Δ::Number=1
 ) where {T<:Number}
     physical_space = Vect[FermionParity](0 => 1, 1 => 1)
+    lattice = fill(physical_space, 1, 1)
+
     # on-site
     h0 = TensorMap(zeros, T, physical_space ← physical_space)
     block(h0, FermionParity(1)) .= -μ
@@ -41,5 +44,10 @@ function square_lattice_pwave(
     block(hy, FermionParity(0)) .= [0 Δ*im; -Δ*im 0]
     block(hy, FermionParity(1)) .= [0 -t; -t 0]
 
-    return AnisotropicNNOperator(h0, hx, hy)
+    return PEPSHamiltonian(
+        lattice,
+        (CartesianIndex(1, 1),) => h0,
+        (CartesianIndex(1, 1), CartesianIndex(1, 2)) => hx,
+        (CartesianIndex(1, 1), CartesianIndex(2, 1)) => hy,
+    )
 end
