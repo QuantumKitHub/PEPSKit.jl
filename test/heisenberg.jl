9cc218d94c8e21e26eb9bde0827205670c7cc7fd
Paul Brehmer <paul.brehmer@univie.ac.at>
2024-07-03 17:20:10 +0200

    Fix Heisenberg 2x2 test

--- test/heisenberg.jl
+++ test/heisenberg.jl
@@ -5,12 +5,10 @@ using TensorKit
 using KrylovKit
 using OptimKit
 
-# Initialize parameters
-H = square_lattice_heisenberg()
+# initialize parameters
 χbond = 2
 χenv = 16
-# ctm_alg = CTMRG(; trscheme=truncdim(χenv), tol=1e-10, miniter=4, maxiter=100, verbosity=1)
-ctm_alg = CTMRG(; trscheme=truncdim(χenv), tol=1e-10, miniter=4, maxiter=100, verbosity=2)
+ctm_alg = CTMRG(; trscheme=truncdim(χenv), tol=1e-10, miniter=4, maxiter=100, verbosity=1)
 opt_alg = PEPSOptimize(;
     boundary_alg=ctm_alg,
     optimizer=LBFGS(4; maxiter=100, gradtol=1e-3, verbosity=2),
@@ -21,6 +19,7 @@ opt_alg = PEPSOptimize(;
 
 # initialize states
 Random.seed!(91283219347)
+H = square_lattice_heisenberg()
 psi_init = InfinitePEPS(2, χbond)
 env_init = leading_boundary(CTMRGEnv(psi_init; Venv=ComplexSpace(χenv)), psi_init, ctm_alg);
 
@@ -30,10 +29,11 @@ result = fixedpoint(psi_init, H, opt_alg, env_init)
 @test result.E ≈ -0.6694421 atol = 1e-2
 
 # same test but for 2x2 unit cell
+H_2x2 = square_lattice_heisenberg(; unitcell=(2, 2))
 psi_init_2x2 = InfinitePEPS(2, χbond; unitcell=(2, 2))
 env_init_2x2 = leading_boundary(
     CTMRGEnv(psi_init_2x2; Venv=ComplexSpace(χenv)), psi_init_2x2, ctm_alg
 );
-result_2x2 = fixedpoint(psi_init_2x2, H, opt_alg, env_init_2x2)
+result_2x2 = fixedpoint(psi_init_2x2, H_2x2, opt_alg, env_init_2x2)
 
-@test result_2x2.E ≈ 4 * -0.6694421 atol = 1e-2
+@test result_2x2.E ≈ 4 * result.E atol = 1e-2
