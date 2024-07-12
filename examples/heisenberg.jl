4c643cb6105a4b1848b084e7bae4ae6e3484ad48
leburgel <lander.burgelman@gmail.com>
2024-07-12 11:49:25 +0200

    One more merge artifact

--- examples/heisenberg.jl
+++ examples/heisenberg.jl
@@ -27,6 +27,6 @@ opt_alg = PEPSOptimize(;
 # E/N = −0.6694421, which is a QMC estimate from https://arxiv.org/abs/1101.3281.
 # Of course there is a noticable bias for small χbond and χenv.
 ψ₀ = InfinitePEPS(2, χbond)
-env₀ = leading_boundary(CTMRGEnv(ψ₀, ℂ^χenv), ψ₀, ctmalg)
+env₀ = leading_boundary(CTMRGEnv(ψ₀, ℂ^χenv), ψ₀, ctm_alg)
 result = fixedpoint(ψ₀, H, opt_alg, env₀)
 @show result.E
