bcd27980a5c7f84d0144fae3b580777b4690709a
Paul Brehmer <paul.brehmer@univie.ac.at>
2024-07-15 17:29:05 +0200

    Revert gaugefix.jl test back to old state

--- test/ctmrg/gaugefix.jl
+++ test/ctmrg/gaugefix.jl
@@ -8,7 +8,31 @@ using PEPSKit: ctmrg_iter, gauge_fix, calc_elementwise_convergence
 scalartypes = [Float64, ComplexF64]
 unitcells = [(1, 1), (2, 2), (3, 2)]
 schemes = [:simultaneous, :sequential]
-χ = Dict([(1, 1) => 8, (2, 2) => 26, (3, 2) => 26])  # Increase χ to converge non-symmetric environments
+χ = 6
+atol = 1e-4
+verbosity = 2
+
+function _make_symmetric(psi)
+    if ==(size(psi)...)
+        return PEPSKit.symmetrize(psi, PEPSKit.Full())
+    else
+        return PEPSKit.symmetrize(PEPSKit.symmetrize(psi, PEPSKit.Depth()), PEPSKit.Width())
+    end
+end
+
+# If I can't make the rng seed behave, I'll just randomly define a peps somehow
+function semi_random_peps!(psi::InfinitePEPS)
+    i = 0
+    A′ = map(psi.A) do a
+        for (_, b) in blocks(a)
+            l = length(b)
+            b .= reshape(collect((1:l) .+ i), size(b))
+            i += l
+        end
+        return a
+    end
+    return InfinitePEPS(A′)
+end
 
 @testset "Trivial symmetry ($T) - ($unitcell) - ($ctmrgscheme)" for (
     T, unitcell, ctmrgscheme
@@ -17,20 +41,23 @@ schemes = [:simultaneous, :sequential]
 )
     physical_space = ComplexSpace(2)
     peps_space = ComplexSpace(2)
-    ctm_space = ComplexSpace(χ[unitcell])
+    ctm_space = ComplexSpace(χ)
 
-    Random.seed!(29358293852)  # Seed RNG to make random environment consistent
-    psi = InfinitePEPS(randn, T, physical_space, peps_space; unitcell)
+    psi = InfinitePEPS(undef, T, physical_space, peps_space; unitcell)
+    semi_random_peps!(psi)
+    psi = _make_symmetric(psi)
+
+    Random.seed!(987654321)  # Seed RNG to make random environment consistent
     ctm = CTMRGEnv(psi, ctm_space)
 
     alg = CTMRG(;
-        tol=1e-10, maxiter=100, verbosity=2, trscheme=FixedSpaceTruncation(), ctmrgscheme
+        tol=1e-10, maxiter=200, verbosity, trscheme=FixedSpaceTruncation(), ctmrgscheme
     )
 
     ctm = leading_boundary(ctm, psi, alg)
     ctm2, = ctmrg_iter(psi, ctm, alg)
     ctm_fixed, = gauge_fix(ctm, ctm2)
-    @test calc_elementwise_convergence(ctm, ctm_fixed) ≈ 0 atol = 1e-6
+    @test calc_elementwise_convergence(ctm, ctm_fixed) ≈ 0 atol = atol
 end
 
 @testset "Z2 symmetry ($T) - ($unitcell) - ($ctmrgscheme)" for (T, unitcell, ctmrgscheme) in
@@ -39,18 +66,22 @@ end
 )
     physical_space = Z2Space(0 => 1, 1 => 1)
     peps_space = Z2Space(0 => 1, 1 => 1)
-    ctm_space = Z2Space(0 => χ[(1, 1)] ÷ 2, 1 => χ[(1, 1)] ÷ 2)
+    ctm_space = Z2Space(0 => χ ÷ 2, 1 => χ ÷ 2)
 
-    Random.seed!(2938293852938)  # Seed RNG to make random environment consistent
+    psi = InfinitePEPS(undef, T, physical_space, peps_space; unitcell)
+    semi_random_peps!(psi)
+    psi = _make_symmetric(psi)
+
+    Random.seed!(987654321)  # Seed RNG to make random environment consistent
     psi = InfinitePEPS(physical_space, peps_space; unitcell)
     ctm = CTMRGEnv(psi, ctm_space)
 
     alg = CTMRG(;
-        tol=1e-10, maxiter=400, verbosity=2, trscheme=FixedSpaceTruncation(), ctmrgscheme
+        tol=1e-10, maxiter=200, verbosity, trscheme=FixedSpaceTruncation(), ctmrgscheme
     )
 
     ctm = leading_boundary(ctm, psi, alg)
     ctm2, = ctmrg_iter(psi, ctm, alg)
     ctm_fixed, = gauge_fix(ctm, ctm2)
-    @test calc_elementwise_convergence(ctm, ctm_fixed) ≈ 0 atol = 1e-6
+    @test calc_elementwise_convergence(ctm, ctm_fixed) ≈ 0 atol = atol
 end
