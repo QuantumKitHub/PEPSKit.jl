84b638f1ceb827bef2c54270538f9b89cf4f34f3
lkdvos <lukas.devos@ugent.be>
2024-08-21 10:19:19 +0200

    Add docstring LocalOperator

--- src/operators/localoperator.jl
+++ src/operators/localoperator.jl
@@ -1,6 +1,29 @@
 
 # Hamiltonian consisting of local terms
 # -------------------------------------
+"""
+    struct LocalOperator{T<:Tuple,S}
+
+A sum of local operators acting on a lattice. The lattice is stored as a matrix of vector spaces,
+and the terms are stored as a tuple of pairs of indices and operators.
+
+# Fields
+
+- `lattice::Matrix{S}`: The lattice on which the operator acts.
+- `terms::T`: The terms of the operator, stored as a tuple of pairs of indices and operators.
+
+# Constructors
+
+    LocalOperator(lattice::Matrix{S}, terms::Pair...)
+    LocalOperator{T,S}(lattice::Matrix{S}, terms::T) where {T,S} # expert mode
+
+# Examples
+
+```julia
+lattice = fill(ℂ^2, 1, 1) # single-site unitcell
+O1 = LocalOperator(lattice, ((1, 1),) => σx, ((1, 1), (1, 2)) => σx ⊗ σx, ((1, 1), (2, 1)) => σx ⊗ σx)
+```
+"""
 struct LocalOperator{T<:Tuple,S}
     lattice::Matrix{S}
     terms::T
