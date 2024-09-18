e9ee9b92b041d3995d9fceb874e2d514f3169951
Paul Brehmer <paul.brehmer@univie.ac.at>
2024-09-18 16:31:03 +0200

    Update model function to accept symmetric sectors using MPSKitModels

--- src/operators/models.jl
+++ src/operators/models.jl
@@ -1,3 +1,5 @@
+using MPSKitModels
+
 ## Model Hamiltonians
 # -------------------
 
@@ -7,16 +9,18 @@
 Square lattice transverse field Ising model.
 """
 function square_lattice_tf_ising(
-    ::Type{T}=ComplexF64; J=1, h=1, unitcell::Tuple{Int,Int}=(1, 1)
+    elt::Type{T}=ComplexF64,
+    symm::Type{<:Sector}=Trivial;
+    J=1,
+    h=1,
+    unitcell::Tuple{Int,Int}=(1, 1),
 ) where {T<:Number}
-    physical_space = ComplexSpace(2)
-    lattice = fill(physical_space, 1, 1)
-    σx = TensorMap(T[0 1; 1 0], physical_space, physical_space)
-    σz = TensorMap(T[1 0; 0 -1], physical_space, physical_space)
-    hzz = nearest_neighbour_hamiltonian(lattice, -J * σz ⊗ σz)
+    term_zz = rmul!(σᶻᶻ(elt, symm), -J)
+    term_x = rmul!(σˣ(elt, symm), -J * h)
+    lattice = fill(domain(term_x)[1], 1, 1)
+    hzz = nearest_neighbour_hamiltonian(lattice, term_zz)
     return repeat(
-        LocalOperator(lattice, hzz.terms..., (CartesianIndex(1, 1),) => -J * h * σx),
-        unitcell...,
+        LocalOperator(lattice, hzz.terms..., (CartesianIndex(1, 1),) => term_x), unitcell...
     )
 end
 
@@ -27,15 +31,17 @@ Square lattice Heisenberg model.
 By default, this implements a single site unit cell via a sublattice rotation.
 """
 function square_lattice_heisenberg(
-    ::Type{T}=ComplexF64; Jx=-1, Jy=1, Jz=-1, unitcell::Tuple{Int,Int}=(1, 1)
+    elt::Type{T}=ComplexF64,
+    symm::Type{<:Sector}=Trivial;
+    Jx=-1,
+    Jy=1,
+    Jz=-1,
+    unitcell::Tuple{Int,Int}=(1, 1),
 ) where {T<:Number}
-    physical_space = ComplexSpace(2)
-    lattice = fill(physical_space, 1, 1)
-    σx = TensorMap(T[0 1; 1 0], physical_space, physical_space)
-    σy = TensorMap(T[0 im; -im 0], physical_space, physical_space)
-    σz = TensorMap(T[1 0; 0 -1], physical_space, physical_space)
-    H = (Jx * σx ⊗ σx) + (Jy * σy ⊗ σy) + (Jz * σz ⊗ σz)
-    return repeat(nearest_neighbour_hamiltonian(lattice, H / 4), unitcell...)
+    term =
+        rmul!(S_xx(elt, symm), Jx) + rmul!(S_yy(elt, symm), Jy) + rmul!(S_zz(elt, symm), Jz)
+    lattice = fill(domain(term)[1], 1, 1)
+    return repeat(nearest_neighbour_hamiltonian(lattice, term), unitcell...)
 end
 
 """
@@ -46,25 +52,26 @@ Square lattice J₁-J₂ model. The `sublattice` kwarg enables a single site uni
 sublattice rotation.
 """
 function square_lattice_j1j2(
-    ::Type{T}=ComplexF64; J1=1, J2=1, unitcell::Tuple{Int,Int}=(1, 1), sublattice=true
+    elt::Type{T}=ComplexF64,
+    symm::Type{<:Sector}=Trivial;
+    J1=1,
+    J2=1,
+    unitcell::Tuple{Int,Int}=(1, 1),
+    sublattice=true,
 ) where {T<:Number}
-    physical_space = ComplexSpace(2)
-    lattice = fill(physical_space, 1, 1)
-    σx = TensorMap(T[0 1; 1 0], physical_space, physical_space)
-    σy = TensorMap(T[0 im; -im 0], physical_space, physical_space)
-    σz = TensorMap(T[1 0; 0 -1], physical_space, physical_space)
-    h_AA = σx ⊗ σx + σy ⊗ σy + σz ⊗ σz
-    h_AB = sublattice ? -σx ⊗ σx + σy ⊗ σy - σz ⊗ σz : h_AA  # Apply sublattice rotation
+    term_AA = S_xx(elt, symm) + S_yy(elt, symm) + S_zz(elt, symm)
+    term_AB = sublattice ? -S_xx(elt, symm) + S_yy(elt, symm) - S_zz(elt, symm) : term_AA  # Apply sublattice rotation
+    lattice = fill(domain(term_AA)[1], 1, 1)
 
     terms = []
     for I in eachindex(IndexCartesian(), lattice)
         nearest_x = I + CartesianIndex(1, 0)
         nearest_y = I + CartesianIndex(0, 1)
         next_xy = I + CartesianIndex(1, 1)
-        push!(terms, (I, nearest_x) => J1 / 4 * h_AB)
-        push!(terms, (I, nearest_y) => J1 / 4 * h_AB)
-        push!(terms, (I, next_xy) => J2 / 4 * h_AA)
-        push!(terms, (nearest_x, nearest_y) => J2 / 4 * h_AA)
+        push!(terms, (I, nearest_x) => J1 * term_AB)
+        push!(terms, (I, nearest_y) => J1 * term_AB)
+        push!(terms, (I, next_xy) => J2 * term_AA)
+        push!(terms, (nearest_x, nearest_y) => J2 * term_AA)
     end
 
     return repeat(LocalOperator(lattice, terms...), unitcell...)
