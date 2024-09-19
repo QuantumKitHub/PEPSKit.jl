ac7b3f00ade0b295fb200f47fc76201a3163007f
Paul Brehmer <paul.brehmer@univie.ac.at>
2024-09-19 14:24:20 +0200

    Add Rotate symmetrization test

--- test/ctmrg/symmetrization.jl
+++ test/ctmrg/symmetrization.jl
@@ -3,25 +3,6 @@ using PEPSKit
 using PEPSKit: herm_depth, herm_width, _fit_spaces
 using TensorKit
 
-@testset "RotateReflect" for unitcell in [(1, 1), (2, 2), (3, 3)]
-    peps = InfinitePEPS(2, 2; unitcell)
-
-    peps_full = symmetrize!(deepcopy(peps), RotateReflect())
-    @test peps_full ≈ _fit_spaces(rotl90(peps_full), peps_full)
-    @test peps_full ≈ _fit_spaces(rot180(peps_full), peps_full)
-    @test peps_full ≈ _fit_spaces(rotr90(peps_full), peps_full)
-
-    peps_reflect_depth = _fit_spaces(
-        InfinitePEPS(reverse(map(herm_depth, peps_full.A); dims=1)), peps_full
-    )
-    @test peps_full ≈ peps_reflect_depth
-
-    peps_reflect_width = _fit_spaces(
-        InfinitePEPS(reverse(map(herm_width, peps_full.A); dims=2)), peps_full
-    )
-    @test peps_full ≈ peps_reflect_width
-end
-
 @testset "ReflectDepth" for unitcell in [(1, 1), (2, 2), (3, 3)]
     peps = InfinitePEPS(2, 2; unitcell)
 
@@ -40,4 +21,32 @@ end
         InfinitePEPS(reverse(map(herm_width, peps_width.A); dims=2)), peps_width
     )
     @test peps_width ≈ peps_reflect
+end
+
+@testset "Rotate" for unitcell in [(1, 1), (2, 2), (3, 3)]
+    peps = InfinitePEPS(2, 2; unitcell)
+
+    peps_rot = symmetrize!(deepcopy(peps), Rotate())
+    @test peps_rot ≈ _fit_spaces(rotl90(peps_rot), peps_rot)
+    @test peps_rot ≈ _fit_spaces(rot180(peps_rot), peps_rot)
+    @test peps_rot ≈ _fit_spaces(rotr90(peps_rot), peps_rot)
+end
+
+@testset "RotateReflect" for unitcell in [(1, 1), (2, 2), (3, 3)]
+    peps = InfinitePEPS(2, 2; unitcell)
+
+    peps_full = symmetrize!(deepcopy(peps), RotateReflect())
+    @test peps_full ≈ _fit_spaces(rotl90(peps_full), peps_full)
+    @test peps_full ≈ _fit_spaces(rot180(peps_full), peps_full)
+    @test peps_full ≈ _fit_spaces(rotr90(peps_full), peps_full)
+
+    peps_reflect_depth = _fit_spaces(
+        InfinitePEPS(reverse(map(herm_depth, peps_full.A); dims=1)), peps_full
+    )
+    @test peps_full ≈ peps_reflect_depth
+
+    peps_reflect_width = _fit_spaces(
+        InfinitePEPS(reverse(map(herm_width, peps_full.A); dims=2)), peps_full
+    )
+    @test peps_full ≈ peps_reflect_width
 end
