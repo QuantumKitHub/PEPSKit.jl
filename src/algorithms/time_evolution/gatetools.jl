f85dcb040858d55762edda7b55e30fce46f69230
sanderdemeyer <80397440+Sander-De-Meyer@users.noreply.github.com>
2024-12-02 18:24:14 +0100

    add check that all operators are twosite

--- src/algorithms/time_evolution/gatetools.jl
+++ src/algorithms/time_evolution/gatetools.jl
@@ -2,6 +2,7 @@
 Convert Hamiltonian `H` with nearest neighbor terms to `exp(-dt * H)`
 """
 function get_gate(dt::Float64, H::LocalOperator)
+    @assert all([length(op.dom) for (_, op) in H.terms] .== 2) "Only two-body terms allowed"
     return LocalOperator(
         H.lattice, Tuple(sites => exp(-dt * op) for (sites, op) in H.terms)...
     )
