9358a8b14829d130dfc6512793fe3b97992bc380
leburgel <lander.burgelman@gmail.com>
2025-02-11 09:37:25 +0100

    Format

--- test/ctmrg/gradients.jl
+++ test/ctmrg/gradients.jl
@@ -34,14 +34,18 @@ gradmodes = [
         LinSolver(; solver=KrylovKit.BiCGStab(; tol=gradtol), iterscheme=:fixed),
         LinSolver(; solver=KrylovKit.BiCGStab(; tol=gradtol), iterscheme=:diffgauge),
         EigSolver(; solver=KrylovKit.Arnoldi(; tol=gradtol), iterscheme=:fixed),
-        EigSolver(; solver=KrylovKit.Arnoldi(; tol=gradtol, eager=true), iterscheme=:diffgauge),
+        EigSolver(;
+            solver=KrylovKit.Arnoldi(; tol=gradtol, eager=true), iterscheme=:diffgauge
+        ),
     ],
     [  # Only use :diffgauge due to high gauge-sensitivity (perhaps due to small χenv?)
         nothing,
         GeomSum(; tol=gradtol, iterscheme=:diffgauge),
         ManualIter(; tol=gradtol, iterscheme=:diffgauge),
         LinSolver(; solver=KrylovKit.BiCGStab(; tol=gradtol), iterscheme=:diffgauge),
-        EigSolver(; solver=KrylovKit.Arnoldi(; tol=gradtol, eager=true), iterscheme=:diffgauge),
+        EigSolver(;
+            solver=KrylovKit.Arnoldi(; tol=gradtol, eager=true), iterscheme=:diffgauge
+        ),
     ],
 ]
 steps = -0.01:0.005:0.01
