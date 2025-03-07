1becbb48fc6a498bce7c8d60a08c4acdb94c10d6
Lukas Devos <ldevos98@gmail.com>
2025-03-07 07:05:21 -0500

    Defaults markdown formatting updates

--- src/Defaults.jl
+++ src/Defaults.jl
@@ -3,53 +3,58 @@
 
 Module containing default algorithm parameter values and arguments.
 
-# CTMRG
+## CTMRG
 
-- `ctmrg_tol=$(Defaults.ctmrg_tol)`: Tolerance checking singular value and norm convergence.
-- `ctmrg_maxiter=$(Defaults.ctmrg_maxiter)`: Maximal number of CTMRG iterations per run.
-- `ctmrg_miniter=$(Defaults.ctmrg_miniter)`: Minimal number of CTMRG carried out.
-- `ctmrg_alg=:$(Defaults.ctmrg_alg)`: Default CTMRG algorithm variant.
-- `ctmrg_verbosity=$(Defaults.ctmrg_verbosity)`: CTMRG output information verbosity
+- `ctmrg_tol=$(Defaults.ctmrg_tol)` : Tolerance checking singular value and norm convergence.
+- `ctmrg_maxiter=$(Defaults.ctmrg_maxiter)` : Maximal number of CTMRG iterations per run.
+- `ctmrg_miniter=$(Defaults.ctmrg_miniter)` : Minimal number of CTMRG carried out.
+- `ctmrg_alg=:$(Defaults.ctmrg_alg)` : Default CTMRG algorithm variant.
+- `ctmrg_verbosity=$(Defaults.ctmrg_verbosity)` : CTMRG output information verbosity
 
-# SVD forward & reverse
+## SVD forward & reverse
 
-- `trscheme=:$(Defaults.trscheme)`: Truncation scheme for SVDs and other decompositions.
-- `svd_fwd_alg=:$(Defaults.svd_fwd_alg)`: SVD algorithm that is used in the forward pass.
-- `svd_rrule_tol=$(Defaults.svd_rrule_tol)`: Accuracy of SVD reverse-rule.
-- `svd_rrule_min_krylovdim=$(Defaults.svd_rrule_min_krylovdim)`: Minimal Krylov dimension of the reverse-rule algorithm (if it is a Krylov algorithm).
-- `svd_rrule_verbosity=$(Defaults.svd_rrule_verbosity)`: SVD gradient output verbosity.
-- `svd_rrule_alg=:$(Defaults.svd_rrule_alg)`: Reverse-rule algorithm for the SVD gradient.
+- `trscheme=:$(Defaults.trscheme)` : Truncation scheme for SVDs and other decompositions.
+- `svd_fwd_alg=:$(Defaults.svd_fwd_alg)` : SVD algorithm that is used in the forward pass.
+- `svd_rrule_tol=$(Defaults.svd_rrule_tol)` : Accuracy of SVD reverse-rule.
+- `svd_rrule_min_krylovdim=$(Defaults.svd_rrule_min_krylovdim)` : Minimal Krylov dimension of the reverse-rule algorithm (if it is a Krylov algorithm).
+- `svd_rrule_verbosity=$(Defaults.svd_rrule_verbosity)` : SVD gradient output verbosity.
+- `svd_rrule_alg=:$(Defaults.svd_rrule_alg)` : Reverse-rule algorithm for the SVD gradient.
 
-# Projectors
+## Projectors
 
-- `projector_alg=:$(Defaults.projector_alg)`: Default variant of the CTMRG projector algorithm.
-- `projector_verbosity=$(Defaults.projector_verbosity)`: Projector output information verbosity.
+- `projector_alg=:$(Defaults.projector_alg)` : Default variant of the CTMRG projector algorithm.
+- `projector_verbosity=$(Defaults.projector_verbosity)` : Projector output information verbosity.
 
-# Fixed-point gradient
+## Fixed-point gradient
 
-- `gradient_tol=$(Defaults.gradient_tol)`: Convergence tolerance for the fixed-point gradient iteration.
-- `gradient_maxiter=$(Defaults.gradient_maxiter)`: Maximal number of iterations for computing the CTMRG fixed-point gradient.
-- `gradient_verbosity=$(Defaults.gradient_verbosity)`: Gradient output information verbosity.
-- `gradient_linsolver=:$(Defaults.gradient_linsolver)`: Default linear solver for the `LinSolver` gradient algorithm.
-- `gradient_eigsolver=:$(Defaults.gradient_eigsolver)`: Default eigensolver for the `EigSolver` gradient algorithm.
-- `gradient_eigsolver_eager=$(Defaults.gradient_eigsolver_eager)`: Enables `EigSolver` algorithm to finish before the full Krylov dimension is reached.
-- `gradient_iterscheme=:$(Defaults.gradient_iterscheme)`: Scheme for differentiating one CTMRG iteration.
-- `gradient_alg=:$(Defaults.gradient_alg)`: Algorithm variant for computing the gradient fixed-point.
+- `gradient_tol=$(Defaults.gradient_tol)` : Convergence tolerance for the fixed-point gradient iteration.
+- `gradient_maxiter=$(Defaults.gradient_maxiter)` : Maximal number of iterations for computing the CTMRG fixed-point gradient.
+- `gradient_verbosity=$(Defaults.gradient_verbosity)` : Gradient output information verbosity.
+- `gradient_linsolver=:$(Defaults.gradient_linsolver)` : Default linear solver for the `LinSolver` gradient algorithm.
+- `gradient_eigsolver=:$(Defaults.gradient_eigsolver)` : Default eigensolver for the `EigSolver` gradient algorithm.
+- `gradient_eigsolver_eager=$(Defaults.gradient_eigsolver_eager)` : Enables `EigSolver` algorithm to finish before the full Krylov dimension is reached.
+- `gradient_iterscheme=:$(Defaults.gradient_iterscheme)` : Scheme for differentiating one CTMRG iteration.
+- `gradient_alg=:$(Defaults.gradient_alg)` : Algorithm variant for computing the gradient fixed-point.
 
-# Optimization
+## Optimization
 
-- `reuse_env=$(Defaults.reuse_env)`: If `true`, the current optimization step is initialized on the previous environment, otherwise a random environment is used.
-- `optimizer_tol=$(Defaults.optimizer_tol)`: Gradient norm tolerance of the optimizer.
-- `optimizer_maxiter=$(Defaults.optimizer_maxiter)`: Maximal number of optimization steps.
-- `optimizer_verbosity=$(Defaults.optimizer_verbosity)`: Optimizer output information verbosity.
-- `optimizer_alg=:$(Defaults.optimizer_alg)`: Default `OptimKit.OptimizerAlgorithm` for PEPS optimization.
-- `lbfgs_memory=$(Defaults.lbfgs_memory)`: Size of limited memory representation of BFGS Hessian matrix.
+- `reuse_env=$(Defaults.reuse_env)` : If `true`, the current optimization step is initialized on the previous environment, otherwise a random environment is used.
+- `optimizer_tol=$(Defaults.optimizer_tol)` : Gradient norm tolerance of the optimizer.
+- `optimizer_maxiter=$(Defaults.optimizer_maxiter)` : Maximal number of optimization steps.
+- `optimizer_verbosity=$(Defaults.optimizer_verbosity)` : Optimizer output information verbosity.
+- `optimizer_alg=:$(Defaults.optimizer_alg)` : Default `OptimKit.OptimizerAlgorithm` for PEPS optimization.
+- `lbfgs_memory=$(Defaults.lbfgs_memory)` : Size of limited memory representation of BFGS Hessian matrix.
 
-# OhMyThreads scheduler
-- `scheduler=Ref{Scheduler}(...)`: Multi-threading scheduler which can be accessed via `set_scheduler!`.
+## OhMyThreads scheduler
+
+- `scheduler=Ref{Scheduler}(...)` : Multi-threading scheduler which can be accessed via `set_scheduler!`.
 """
 module Defaults
+
+export set_scheduler!
+
 using OhMyThreads
+
 # CTMRG
 const ctmrg_tol = 1e-8
 const ctmrg_maxiter = 100
@@ -91,6 +96,7 @@ const lbfgs_memory = 20
 
 # OhMyThreads scheduler defaults
 const scheduler = Ref{Scheduler}()
+
 """
     set_scheduler!([scheduler]; kwargs...)
 
@@ -125,9 +131,9 @@ function set_scheduler!(sc=OhMyThreads.Implementation.NotGiven(); kwargs...)
     end
     return nothing
 end
-export set_scheduler!
 
 function __init__()
     return set_scheduler!()
 end
+
 end
