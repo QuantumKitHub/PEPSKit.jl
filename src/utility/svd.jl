d761218a1221d58789d38b5611dcd9e17a9a553d
leburgel <lander.burgelman@ugent.be>
2025-03-12 22:20:23 +0100

    Definitely too low

--- src/utility/svd.jl
+++ src/utility/svd.jl
@@ -292,9 +292,7 @@ function ChainRulesCore.rrule(
 
     smallest_sval = minimum(((_, b),) -> minimum(diag(b)), blocks(S̃))
     pullback_tol = clamp(
-        smallest_sval,
-        eps(scalartype(S̃))^(3 / 4),
-        TensorKitCRCExt.default_pullback_gaugetol(S),
+        smallest_sval, eps(scalartype(S̃))^(3 / 4), eps(scalartype(S̃))^(1 / 2)
     )
 
     function tsvd!_nothing_pullback(ΔUSVi)
