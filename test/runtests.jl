4a7c2d1673733ba7b218bc1b53ee3cff70118121
sanderdemeyer <sander.demeyer@ugent.be>
2025-04-24 12:41:46 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,6 +32,9 @@ end
         @time @safetestset "PEPO" begin
             include("ctmrg/pepo.jl")
         end
+        @time @safetestset "PEPOLayers" begin
+            include("ctmrg/pepo_layers.jl")
+        end
     end
     if GROUP == "ALL" || GROUP == "GRADIENTS"
         @time @safetestset "CTMRG gradients" begin
