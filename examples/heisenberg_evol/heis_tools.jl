fd7d25dde01424d165b9b9f8c773b6629850e06f
Yue Zhengyuan <yuezy1997@icloud.com>
2025-04-25 21:00:49 +0800

    Add spin correlation measurement to SU example

--- examples/heisenberg_evol/heis_tools.jl
+++ examples/heisenberg_evol/heis_tools.jl
@@ -8,7 +8,7 @@ import Statistics: mean
 
 module MeasureHeis
 
-export measure_heis
+export cal_spincor, measure_heis
 
 using TensorKit
 import MPSKitModels: S_x, S_y, S_z, S_exchange
@@ -40,6 +40,31 @@ function cal_mags(peps::InfinitePEPS, env::CTMRGEnv)
 end
 
 """
+Measure spin correlation ⟨Sᵢ⋅Sⱼ⟩ on each nearest neighbor bond
+"""
+function cal_spincor(peps::InfinitePEPS, env::CTMRGEnv)
+    Nr, Nc = size(peps)
+    symm = sectortype(peps.A[1, 1])
+    op = real(S_exchange(ComplexF64, symm))
+    lattice = collect(space(t, 1) for t in peps.A)
+    corHs = collect(
+        expectation_value(
+            peps,
+            LocalOperator(lattice, (CartesianIndex(r, c), CartesianIndex(r, c + 1)) => op),
+            env,
+        ) for (r, c) in Iterators.product(1:Nr, 1:Nc)
+    )
+    corVs = collect(
+        expectation_value(
+            peps,
+            LocalOperator(lattice, (CartesianIndex(r, c), CartesianIndex(r - 1, c)) => op),
+            env,
+        ) for (r, c) in Iterators.product(1:Nr, 1:Nc)
+    )
+    return corHs, corVs
+end
+
+"""
 Measure physical quantities for Heisenberg model
 """
 function measure_heis(peps::InfinitePEPS, H::LocalOperator, env::CTMRGEnv)
@@ -51,6 +76,7 @@ function measure_heis(peps::InfinitePEPS, H::LocalOperator, env::CTMRGEnv)
         norm([mags[r, c] for mags in results["mag"]]) for
         (r, c) in Iterators.product(1:Nr, 1:Nc)
     )
+    results["corH"], results["corV"] = cal_spincor(peps, env)
     return results
 end
 
