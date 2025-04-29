c1744a1f5e58c77bb0054c818c45f0bb26348531
Paul Brehmer <paul.brehmer@univie.ac.at>
2025-04-29 11:52:16 +0200

    Fix make.jl root to repair Cache.toml

--- examples/make.jl
+++ examples/make.jl
@@ -90,7 +90,7 @@ function build_example(root, name)
     end
 end
 
-function build(root="")
+function build(root="./")
     return cd(@__DIR__) do
         examples = filter(isdir, readdir(joinpath(@__DIR__, root))) # filter out directories to ignore Cache.toml, etc.
         return map(ex -> build_example(root, ex), examples)
