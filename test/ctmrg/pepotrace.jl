868de2eb0f5385459fc2ea435c4b48c85415cce7
sanderdemeyer <sander.demeyer@ugent.be>
2025-05-22 10:51:01 +0200

    update tests
    check network_value instead of mpo
    update convention of tests

--- test/ctmrg/pepotrace.jl
+++ test/ctmrg/pepotrace.jl
@@ -19,19 +19,14 @@ envspace = Vect[I](0 => χenv, 1 => χenv)
 Random.seed!(97646475)
 
 # Construct random PEPO tensors
-perturbation = 1e-2
-# unit = permute(id(T, pspace ⊗ vspace ⊗ vspace), ((1,4),(5,6,2,3)))
 O = randn(T, pspace ⊗ pspace', vspace ⊗ vspace ⊗ vspace' ⊗ vspace')
-# O = unit + perturbation * O / norm(O)
 O = O + twist(flip(PEPSKit._dag(O), 3:6), [4 6])
 M = randn(T, pspace ⊗ pspace', vspace ⊗ vspace ⊗ vspace' ⊗ vspace')
 M = M + twist(flip(PEPSKit._dag(M), 3:6), [4 6])
 
 # Fuse a layer consisting of O-O and MO together
 fuser = isomorphism(T, vspace ⊗ vspace, fuse(vspace, vspace))
-# fuser_conj = isometry(T, vspace' ⊗ vspace', fuse(vspace', vspace')')
 fuser_adj = isomorphism(T, vspace' ⊗ vspace, fuse(vspace', vspace))
-# fuser_adj_conj = isometry(T, vspace ⊗ vspace', fuse(vspace, vspace')')
 @tensor O2[-1 -2; -3 -4 -5 -6] :=
     O[-1 1; 2 4 6 8] *
     O[1 -2; 3 5 7 9] *
@@ -49,7 +44,6 @@ fuser_adj = isomorphism(T, vspace' ⊗ vspace, fuse(vspace', vspace))
 
 # Create `InfiniteSquareNetwork`s of of both options
 O_stack = fill(O, 1, 1, 2)
-# O_stack[1, 1, 2] = twist(O, 2)
 network = InfiniteSquareNetwork(InfinitePEPO(O_stack));
 network_fused = InfiniteSquareNetwork(InfinitePEPO(O2));
 
@@ -69,11 +63,11 @@ network_fused = InfiniteSquareNetwork(InfinitePEPO(O2));
     fuser[4 5; -4] *
     conj(fuser[6 7; -5]) *
     conj(fuser[8 9; -6])
-ψ′ = randn(T, pspace, vspace ⊗ vspace ⊗ vspace' ⊗ vspace')
-ψ′ = ψ′ / norm(ψ′)
-@tensor Odagψ′[-1; -3 -4 -5 -6] :=
+ϕ = randn(T, pspace, vspace ⊗ vspace ⊗ vspace' ⊗ vspace')
+ϕ = ϕ / norm(ϕ)
+@tensor Odagϕ[-1; -3 -4 -5 -6] :=
     twist(PEPSKit._dag(O), 1:4)[-1 1; 2 4 6 8] *
-    ψ′[1; 3 5 7 9] *
+    ϕ[1; 3 5 7 9] *
     fuser_adj[2 3; -3] *
     fuser_adj[4 5; -4] *
     conj(fuser_adj[6 7; -5]) *
@@ -93,13 +87,22 @@ network_fused_OO = InfinitePEPS(Oψ)
 ctm_styles = [:sequential, :simultaneous]
 projector_algs = [:halfinfinite, :fullinfinite]
 
-@testset "mpotensor for PEPOTraceSandwich" begin
+@testset "mpotensor for PEPOTraceSandwich using $alg with $projector_alg" for (
+    alg, projector_alg
+) in Iterators.product(
+    ctm_styles, projector_algs
+)
     # Test whether the mpotensor of a double layer of PEPOs is the same as in the case where the layers are fused
     mpo = InfiniteSquareNetwork(map(PEPSKit.mpotensor, PEPSKit.unitcell(network)))
     mpo_fused = InfiniteSquareNetwork(
         map(PEPSKit.mpotensor, PEPSKit.unitcell(network_fused))
     )
-    @test mpo ≈ mpo_fused
+    env, = leading_boundary(CTMRGEnv(mpo, envspace), mpo; alg, maxiter=250, projector_alg)
+    env_fused, = leading_boundary(
+        CTMRGEnv(mpo_fused, envspace), mpo_fused; alg, maxiter=250, projector_alg
+    )
+
+    @test network_value(mpo, env) ≈ network_value(mpo_fused, env_fused)
 end
 
 @testset "PEPO layers CTMRG contraction using $alg with $projector_alg" for (
@@ -126,30 +129,44 @@ end
 projector_alg = projector_algs[1] # only use :halfinfinite for this test due to convergence issues
 @testset "Test adjoint of an InfinitePEPO using $alg with $projector_alg" for alg in
                                                                               ctm_styles
-    # Test the definition of the adjoint of an operator, i.e. <Oψ₁, ψ₂> = <ψ₁, adjoint(O)ψ₂>, with Oψ the physical action of the PEPO O on the PEPS ψ.
-    network_Oψψ′ = InfiniteSquareNetwork(InfinitePEPS(Oψ), InfinitePEPS(ψ′))
-    network_ψOdagψ′ = InfiniteSquareNetwork(InfinitePEPS(ψ), InfinitePEPS(Odagψ′))
+    # Test the definition of the adjoint of an operator, i.e. dot(psi, O, phi) == dot(psi, O * phi) == dot(O' * psi, phi) for any two states psi and phi.
+    network_ψOϕ = InfiniteSquareNetwork(
+        InfinitePEPS(ψ), InfinitePEPO(PEPSKit._dag(O)), InfinitePEPS(ϕ)
+    )
+    network_ψ_Odagϕ = InfiniteSquareNetwork(InfinitePEPS(ψ), InfinitePEPS(Odagϕ))
+    network_Oψ_ϕ = InfiniteSquareNetwork(InfinitePEPS(Oψ), InfinitePEPS(ϕ))
 
-    env_Oψψ′, = leading_boundary(
-        CTMRGEnv(network_Oψψ′, envspace),
-        network_Oψψ′;
+    env_ψOϕ, = leading_boundary(
+        CTMRGEnv(network_ψOϕ, envspace),
+        network_ψOϕ;
         alg,
         maxiter=500,
         projector_alg,
         verbosity=2,
     )
-    env_ψOdagψ′, = leading_boundary(
-        CTMRGEnv(network_ψOdagψ′, envspace),
-        network_ψOdagψ′;
+    env_ψ_Odagϕ, = leading_boundary(
+        CTMRGEnv(network_ψ_Odagϕ, envspace),
+        network_ψ_Odagϕ;
         alg,
         maxiter=500,
         projector_alg,
         verbosity=2,
     )
-    overlap1 = network_value(network_Oψψ′, env_Oψψ′)
-    overlap2 = network_value(network_ψOdagψ′, env_ψOdagψ′)
+    env_Oψ_ϕ, = leading_boundary(
+        CTMRGEnv(network_Oψ_ϕ, envspace),
+        network_Oψ_ϕ;
+        alg,
+        maxiter=500,
+        projector_alg,
+        verbosity=2,
+    )
+
+    overlap1 = network_value(network_ψOϕ, env_ψOϕ)
+    overlap2 = network_value(network_ψ_Odagϕ, env_ψ_Odagϕ)
+    overlap3 = network_value(network_Oψ_ϕ, env_Oψ_ϕ)
 
     @test overlap1 ≈ overlap2 atol = 5e-3
+    @test overlap1 ≈ overlap3 atol = 5e-3
 end
 
 @testset "PEPO layers CTMRG contraction with its adjoint using $alg with $projector_alg" for (
