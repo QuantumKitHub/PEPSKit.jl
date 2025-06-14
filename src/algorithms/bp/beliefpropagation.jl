96c3535faf4a93b93b0c9569b775d5b7d2dd0b27
Lukas Devos <ldevos98@gmail.com>
2025-06-14 14:18:55 -0400

    Add BP fixed point

--- src/algorithms/bp/beliefpropagation.jl
+++ src/algorithms/bp/beliefpropagation.jl
@@ -1,5 +1,34 @@
 @kwdef struct BeliefPropagation
     maxiter::Int = 10
+    tol::Float64 = 1e-6
+    verbosity::Int = 2
+end
+
+function bp_fixedpoint(env::BPEnv, network::InfiniteSquareNetwork, alg::BeliefPropagation)
+    log = MPSKit.IterLog("BP")
+    ϵ = Inf
+
+    return LoggingExtras.withlevel(; alg.verbosity) do
+        @infov 1 loginit!(log, ϵ)
+        iter = 0
+        while true
+            iter += 1
+            env′ = bp_iteration(network, env, alg)
+            ϵ = oftype(ϵ, tr_distance(env, env′))
+            env = env′
+
+            if ϵ < alg.tol
+                @infov 2 logfinish!(log, iter, ϵ)
+                return env, ϵ
+            end
+            if iter ≥ alg.maxiter
+                @warnv 1 logcancel!(log, iter, ϵ)
+                return env, ϵ
+            end
+
+            @infov 3 logiter!(log, iter, ϵ)
+        end
+    end
 end
 
 function bp_iteration(network::InfiniteSquareNetwork, env::BPEnv, alg::BeliefPropagation)
@@ -42,4 +71,17 @@ function update_message(I::CartesianIndex{3}, network::InfiniteSquareNetwork, en
     else
         throw(ArgumentError("Invalid direction $dir"))
     end
+end
+
+function tr_distance(A::BPEnv, B::BPEnv)
+    return sum(zip(A.messages, B.messages)) do (a, b)
+        return trnorm(add(a, b, -inv(tr(b)), inv(tr(a))))
+    end
+end
+
+function trnorm(M::AbstractTensorMap, p::Real=1)
+    return TensorKit._norm(svdvals(M), p, zero(real(scalartype(M))))
+end
+function trnorm!(M::AbstractTensorMap, p::Real=1)
+    return TensorKit._norm(svdvals!(M), p, zero(real(scalartype(M))))
 end
