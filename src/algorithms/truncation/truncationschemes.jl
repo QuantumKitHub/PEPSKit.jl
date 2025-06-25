66f56f10cedbc3414ea1f8079f950361d8f7086d
Sander De Meyer <74001142+sanderdemeyer@users.noreply.github.com>
2025-06-25 05:55:02 +0200

    Add SiteDependentTruncation (#211)
    * add anisotropic Simple Update
    
    * fix 3site su
    
    * make SiteDependentTruncation a new truncation scheme
    
    Simple update was changed accordingly, CTMRG not yet
    
    * update on arguments and constructors
    
    * Fix 3-site SU for SiteDependentTruncationScheme
    
    * small documentation update
    
    * Change name and add rotl90
    
    * fix export
    
    * change arguments in test
    
    * add test for 3-site cluster update
    
    * change test to increase patch coverage
    
    * change name again
    
    * small updates
    
    * rewrite mirror_antidiag
    
    * add test on non-square unit cell and fix bug
    
    * make the new test a separate test to test bipartite=true
    
    * Restore old Heisenberg SU-AD test
    
    * Add test of SU with SiteDependentTruncation
    
    * Add TODO on bipartite check for SiteDependentTruncation
    
    * Update src/algorithms/time_evolution/simpleupdate3site.jl
    
    Co-authored-by: Yue Zhengyuan <yuezy1997@icloud.com>
    
    * remove redundant TensorKit. before TruncationScheme
    
    * remove redundant TensorKit. before TruncationScheme bis
    
    * remove redundant where clauses
    
    * Add selection support with symbol
    
    * Change back constant
    
    * Also simplify rotation
    
    * Remove test on Hubbard SU with SiteDependentTruncation
    
    ---------
    
    Co-authored-by: Yue Zhengyuan <yuezy1997@icloud.com>
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>

--- src/algorithms/truncation/truncationschemes.jl
+++ src/algorithms/truncation/truncationschemes.jl
@@ -5,7 +5,11 @@ CTMRG specific truncation scheme for `tsvd` which keeps the bond space on which 
 is performed fixed. Since different environment directions and unit cell entries might
 have different spaces, this truncation style is different from `TruncationSpace`.
 """
-struct FixedSpaceTruncation <: TensorKit.TruncationScheme end
+struct FixedSpaceTruncation <: TruncationScheme end
+
+struct SiteDependentTruncation{T<:TruncationScheme} <: TruncationScheme
+    trschemes::Array{T,3}
+end
 
 const TRUNCATION_SCHEME_SYMBOLS = IdDict{Symbol,Type{<:TruncationScheme}}(
     :fixedspace => FixedSpaceTruncation,
@@ -14,6 +18,7 @@ const TRUNCATION_SCHEME_SYMBOLS = IdDict{Symbol,Type{<:TruncationScheme}}(
     :truncdim => TensorKit.TruncationDimension,
     :truncspace => TensorKit.TruncationSpace,
     :truncbelow => TensorKit.TruncationCutoff,
+    :sitedependent => SiteDependentTruncation,
 )
 
 # Should be TruncationScheme but rename to avoid type piracy
@@ -24,4 +29,66 @@ function _TruncationScheme(; alg=Defaults.trscheme, η=nothing)
     alg_type = TRUNCATION_SCHEME_SYMBOLS[alg]
 
     return isnothing(η) ? alg_type() : alg_type(η)
+end
+
+function truncation_scheme(
+    trscheme::TruncationScheme, direction::Int, row::Int, col::Int; kwargs...
+)
+    return trscheme
+end
+
+function truncation_scheme(
+    trscheme::SiteDependentTruncation, direction::Int, row::Int, col::Int;
+)
+    return trscheme.trschemes[direction, row, col]
+end
+
+# Mirror a TruncationScheme by its anti-diagonal line.
+# When the number of directions is 2, it swaps the first and second direction, consistent with xbonds and ybonds, respectively.
+# When the number of directions is 4, it swaps the first and second, and third and fourth directions, consistent with the order NORTH, EAST, SOUTH, WEST.
+mirror_antidiag(trscheme::TruncationScheme) = trscheme
+function mirror_antidiag(trscheme::SiteDependentTruncation)
+    directions = size(trscheme.trschemes)[1]
+    if directions == 2
+        trschemes_mirrored = stack(
+            (
+                mirror_antidiag(trscheme.trschemes[EAST, :, :]),
+                mirror_antidiag(trscheme.trschemes[NORTH, :, :]),
+            );
+            dims=1,
+        )
+    elseif directions == 4
+        trschemes_mirrored = stack((
+            mirror_antidiag(trscheme.trschemes[EAST, :, :]),
+            mirror_antidiag(trscheme.trschemes[NORTH, :, :]),
+            mirror_antidiag(trscheme.trschemes[WEST, :, :]),
+            mirror_antidiag(trscheme.trschemes[SOUTH, :, :]),
+        ))
+    else
+        error("Unsupported number of directions for mirror_antidiag: $directions")
+    end
+    return SiteDependentTruncation(trschemes_mirrored)
+end
+
+# TODO: type piracy
+Base.rotl90(trscheme::TruncationScheme) = trscheme
+
+function Base.rotl90(trscheme::SiteDependentTruncation)
+    directions, rows, cols = size(trscheme.trschemes)
+    trschemes_rotated = similar(trscheme.trschemes, directions, cols, rows)
+
+    if directions == 2
+        trschemes_rotated[NORTH, :, :] = circshift(
+            rotl90(trscheme.trschemes[EAST, :, :]), (0, -1)
+        )
+        trschemes_rotated[EAST, :, :] = rotl90(trscheme.trschemes[NORTH, :, :])
+    elseif directions == 4
+        for dir in 1:4
+            dir′ = _prev(dir, 4)
+            trschemes_rotated[dir′, :, :] = rotl90(trscheme.trschemes[dir, :, :])
+        end
+    else
+        throw(ArgumentError("Unsupported number of directions for rotl90: $directions"))
+    end
+    return SiteDependentTruncation(trschemes_rotated)
 end
