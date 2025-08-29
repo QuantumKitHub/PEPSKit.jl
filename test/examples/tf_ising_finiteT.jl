6bac02dd9cdf7f002845a57b4954e2bc5efdd1c8
Yue Zhengyuan <yuezy1997@icloud.com>
2025-08-29 17:34:04 +0800

    Fix CTMRG convergence in test

--- test/examples/tf_ising_finiteT.jl
+++ test/examples/tf_ising_finiteT.jl
@@ -6,14 +6,14 @@ using MPSKitModels
 using PEPSKit
 using PEPSKit: LocalOperator
 
-Random.seed!(453796)
+Random.seed!(10235876)
 σx = σˣ(Float64, Trivial)
 σz = σᶻ(Float64, Trivial)
 
 # Benchmark data of [σx, σz] from HOTRG
 # Physical Review B 86, 045139 (2012) Fig. 15-16
 bm_β = [0.5632, 0.0]
-bm_2β = [0.5297, -0.8265]
+bm_2β = [0.5297, 0.8265]
 
 # only contains 2-site gates, convenient for time evolution
 function tfising_model(T::Type{<:Number}, lattice::InfiniteSquare; J = 1.0, g = 1.0)
@@ -24,6 +24,15 @@ function tfising_model(T::Type{<:Number}, lattice::InfiniteSquare; J = 1.0, g = 
     spaces = fill(pspace, (lattice.Nrows, lattice.Ncols))
     gate = ZZ + (unit ⊗ X + X ⊗ unit) / 4
     return PEPSKit.nearest_neighbour_hamiltonian(spaces, gate)
+end
+
+function converge_env(state, χ::Int)
+    trscheme1 = truncdim(4) & truncerr(1.0e-12)
+    env0 = CTMRGEnv(randn, Float64, state, ℂ^4)
+    env, = leading_boundary(env0, state; trscheme = trscheme1, tol = 1.0e-10)
+    trscheme2 = truncdim(χ) & truncerr(1.0e-12)
+    env, = leading_boundary(env, state; trscheme = trscheme2, tol = 1.0e-10)
+    return env
 end
 
 function measure_mag(pepo::InfinitePEPO, pf::InfinitePartitionFunction, env::CTMRGEnv)
@@ -49,7 +58,6 @@ pepo0 = trivial_InfinitePEPO(Float64, ℂ^2, (Nr, Nc, 1))
 wts0 = SUWeight(pepo0)
 
 trscheme_pepo = truncdim(8) & truncerr(1.0e-12)
-trscheme_env = truncdim(16) & truncerr(1.0e-12)
 
 ham = tfising_model(Float64, InfiniteSquare(Nr, Nc); J = 1.0, g = 2.0)
 dt, maxiter = 1.0e-3, 400
@@ -62,25 +70,23 @@ alg = SimpleUpdate(dt, 0.0, maxiter, trscheme_pepo)
 ## results at β, or T = 2.5
 pepo, wts, = simpleupdate(pepo0, ham, alg, wts0; gate_side = :both)
 pf = InfinitePartitionFunction(pepo)
-env0 = CTMRGEnv(randn, Float64, pf, ℂ^16)
-env, = leading_boundary(env0, pf; trscheme = trscheme_env, tol = 1.0e-10)
+env = converge_env(pf, 16)
 result_β = measure_mag(pepo, pf, env)
 @info "Magnetization at T = $(1 / β)" result_β
-@test isapprox(result_β, bm_β, rtol = 1.0e-2)
+@test isapprox(abs.(result_β), bm_β, rtol = 1.0e-2)
 
 ## results at 2β, or T = 1.25
 pepo, wts, = simpleupdate(pepo, ham, alg, wts; gate_side = :both)
 pf = InfinitePartitionFunction(pepo)
-env, = leading_boundary(env, pf; trscheme = trscheme_env, tol = 1.0e-10)
+env = converge_env(pf, 16)
 result_2β = measure_mag(pepo, pf, env)
 @info "Magnetization at T = $(1 / (2β))" result_2β
-@test isapprox(result_2β, bm_2β, rtol = 1.0e-4)
+@test isapprox(abs.(result_2β), bm_2β, rtol = 1.0e-4)
 
 # purification approach (should match 2β result)
 pepo, = simpleupdate(pepo0, ham, alg, wts0; gate_side = :codomain)
 peps = InfinitePEPS(pepo)
-env0 = CTMRGEnv(randn, Float64, peps, ℂ^8)
-env, = leading_boundary(env0, peps; trscheme = truncdim(8) & truncerr(1.0e-12))
+env = converge_env(peps, 8)
 result_2β′ = measure_mag(peps, env)
 @info "Magnetization at T = $(1 / (2β)) (purification approach)" result_2β′
-@test isapprox(result_2β′, bm_2β, rtol = 1.0e-2)
+@test isapprox(abs.(result_2β′), bm_2β, rtol = 1.0e-2)
