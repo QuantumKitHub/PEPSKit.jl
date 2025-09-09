8c936d46fe38cb0c784e0342d96c817820c92248
Lukas Devos <ldevos98@gmail.com>
2025-09-09 19:20:33 -0400

    Generalize to single-layer densitymatrix expectation values

--- test/toolbox/densitymatrices.jl
+++ test/toolbox/densitymatrices.jl
@@ -7,6 +7,35 @@ ds = Dict(Trivial => ℂ^2, U1Irrep => U1Space(i => d for (i, d) in zip(-1:1, (1
 Ds = Dict(Trivial => ℂ^3, U1Irrep => U1Space(i => D for (i, D) in zip(-1:1, (1, 2, 2))), FermionParity => Vect[FermionParity](0 => 3, 1 => 2))
 χs = Dict(Trivial => ℂ^4, U1Irrep => U1Space(i => χ for (i, χ) in zip(-2:2, (1, 3, 2))), FermionParity => Vect[FermionParity](0 => 3, 1 => 2))
 
+@testset "Single-layer densitymatrix contractions ($I)" for I in keys(ds)
+    d = ds[I]
+    D = Ds[I]
+    χ = χs[I]
+    ρ = InfinitePEPO(d, D)
+
+    ρ_pf = @constinferred InfinitePartitionFunction(ρ)
+    env = CTMRGEnv(ρ_pf, χ)
+
+    O = rand(d, d)
+    @plansor O_pf[W S; N E] := O[p'; p] * ρ[1, 1, 1][p p'; N E S W]
+
+    # Single site
+    O_singlesite = LocalOperator(reshape(physicalspace(ρ), 1, 1), ((1, 1),) => O)
+    E1 = expectation_value(ρ, O_singlesite, env)
+    E2 = expectation_value(ρ_pf, CartesianIndex(1, 1) => O_pf, env)
+    @test E1 ≈ E2
+
+    # two sites
+    for inds in zip(
+            [(1, 1), (1, 1), (1, 1), (1, 2), (1, 1)],
+            [(2, 1), (1, 2), (2, 2), (2, 1), (3, 1)]
+        )
+        O_twosite = LocalOperator(reshape(physicalspace(ρ), 1, 1), inds => O ⊗ O)
+        E3 = expectation_value(ρ, O_twosite, env)
+        # TODO: not defined for partition functions...
+    end
+end
+
 @testset "Double-layer densitymatrix contractions ($I)" for I in keys(ds)
     d = ds[I]
     D = Ds[I]
