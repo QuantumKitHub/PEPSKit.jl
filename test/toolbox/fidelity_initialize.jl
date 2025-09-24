61ba01f8f436b51448bc4f6869610542fdc64d8c
Paul Brehmer <paul.brehmer@univie.ac.at>
2025-09-24 18:53:29 +0200

    Add small `approximate` test

--- test/toolbox/fidelity_initialize.jl
+++ test/toolbox/fidelity_initialize.jl
@@ -29,6 +29,25 @@ using Test
     @test size(peps_single2) == (1, 1)
 end
 
+@testset "approximate methods with FidelityMaxCrude" begin
+    peps1 = InfinitePEPS(2, 2)
+    peps2 = InfinitePEPS(2, 2)
+    Random.seed!(1234)
+    peps_approx1, = approximate(
+        peps1, peps2; maxiter = 3,
+        boundary_alg = (; trscheme = truncdim(6), verbosity = 1)
+    )
+    Random.seed!(1234)
+    peps_approx2, = approximate(
+        peps1, peps2, FidelityMaxCrude(;
+            maxiter = 3,
+            boundary_alg = (; trscheme = truncdim(6), verbosity = 1)
+        )
+    )
+
+    @test peps_approx1 ≈ peps_approx2
+end
+
 @testset "_spacemax for non-uniform unit cell and symmetry sectors" begin
     Pspaces = fill(Z2Space(0 => 1, 1 => 1), (2, 2))
     Nspaces = [
