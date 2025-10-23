88ced8924e28685735eba05d75c8588cd7f7dd52
Yue Zhengyuan <yuezy1997@icloud.com>
2025-10-23 22:57:14 +0800

    Update docstring

--- src/algorithms/time_evolution/time_evolve.jl
+++ src/algorithms/time_evolution/time_evolve.jl
@@ -19,7 +19,5 @@ the time span `dt * nstep` based on Trotter decomposition.
     instead, (i.e. ``\\exp(-Hdt)`` instead of ``\\exp(-iHdt)``). This can be useful for using this
     function to compute the ground state of a Hamiltonian, or to compute finite-temperature
     properties of a system.
-- `tol::Float64 = 0.0`: Tolerance of the change in SUWeight (for simple update; default 1.0e-9) 
-    or energy (for full update; default 1.0e-8) to determine if the ground state search has converged.
 """
 time_evolve
