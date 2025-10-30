a25b06fbc8fd6fcd1287dd6b139f02325effd839
Yue Zhengyuan <yuezy1997@icloud.com>
2025-10-30 16:22:26 +0800

    Test correlators with dual physical space

--- test/utility/correlator.jl
+++ test/utility/correlator.jl
@@ -9,12 +9,11 @@ function get_spaces(sym::Type{<:Sector})
     @assert sym in syms
     Nr, Nc = 2, 2
     Vphy = Vect[sym](0 => 1, 1 => 1)
-    Pspaces = fill(Vphy, (Nr, Nc))
     V = Vect[sym](0 => 1, 1 => 2)
     Venv = Vect[sym](0 => 2, 1 => 2)
     Nspaces = [V' V; V V']
     Espaces = [V V'; V' V]
-    return Vphy, Venv, Pspaces, Nspaces, Espaces
+    return Vphy, Venv, Nspaces, Espaces
 end
 
 site0 = CartesianIndex(1, 1)
@@ -24,37 +23,43 @@ site1ys = collect(site0 + CartesianIndex(i, 0) for i in 2:2:maxsep)
 
 @testset "Correlator in InfinitePEPS ($(sym))" for sym in syms
     Random.seed!(100)
-    Vphy, Venv, Pspaces, Nspaces, Espaces = get_spaces(sym)
-    op = randn(ComplexF64, Vphy ⊗ Vphy → Vphy ⊗ Vphy)
-    peps = InfinitePEPS(randn, ComplexF64, Pspaces, Nspaces, Espaces)
-    env = CTMRGEnv(randn, ComplexF64, peps, Venv)
-    for site1s in (site1xs, site1ys)
-        vals1 = correlator(peps, op, site0, site1s, env)
-        vals2 = map(site1s) do site1
-            O = LocalOperator(Pspaces, (site0, site1) => op)
-            return expectation_value(peps, O, env)
+    Vphy, Venv, Nspaces, Espaces = get_spaces(sym)
+    for Vp in [Vphy', Vphy]
+        op = randn(ComplexF64, Vp ⊗ Vp → Vp ⊗ Vp)
+        Pspaces = fill(Vp, size(Nspaces))
+        peps = InfinitePEPS(randn, ComplexF64, Pspaces, Nspaces, Espaces)
+        env = CTMRGEnv(randn, ComplexF64, peps, Venv)
+        for site1s in (site1xs, site1ys)
+            vals1 = correlator(peps, op, site0, site1s, env)
+            vals2 = map(site1s) do site1
+                O = LocalOperator(Pspaces, (site0, site1) => op)
+                return expectation_value(peps, O, env)
+            end
+            @info vals1
+            @info vals2
+            @test vals1 ≈ vals2
         end
-        @info vals1
-        @info vals2
-        @test vals1 ≈ vals2
     end
 end
 
 @testset "Correlator in 1-layer InfinitePEPO ($(sym))" for sym in syms
     Random.seed!(100)
-    Vphy, Venv, Pspaces, Nspaces, Espaces = get_spaces(sym)
-    op = randn(ComplexF64, Vphy ⊗ Vphy → Vphy ⊗ Vphy)
-    pepo = InfinitePEPO(randn, ComplexF64, Pspaces, Nspaces, Espaces)
-    pf = InfinitePartitionFunction(pepo)
-    env = CTMRGEnv(randn, ComplexF64, pf, Venv)
-    for site1s in (site1xs, site1ys)
-        vals1 = correlator(pepo, op, site0, site1s, env)
-        vals2 = map(site1s) do site1
-            O = LocalOperator(Pspaces, (site0, site1) => op)
-            return expectation_value(pepo, O, env)
+    Vphy, Venv, Nspaces, Espaces = get_spaces(sym)
+    for Vp in [Vphy', Vphy]
+        op = randn(ComplexF64, Vp ⊗ Vp → Vp ⊗ Vp)
+        Pspaces = fill(Vp, size(Nspaces))
+        pepo = InfinitePEPO(randn, ComplexF64, Pspaces, Nspaces, Espaces)
+        pf = InfinitePartitionFunction(pepo)
+        env = CTMRGEnv(randn, ComplexF64, pf, Venv)
+        for site1s in (site1xs, site1ys)
+            vals1 = correlator(pepo, op, site0, site1s, env)
+            vals2 = map(site1s) do site1
+                O = LocalOperator(Pspaces, (site0, site1) => op)
+                return expectation_value(pepo, O, env)
+            end
+            @info vals1
+            @info vals2
+            @test vals1 ≈ vals2
         end
-        @info vals1
-        @info vals2
-        @test vals1 ≈ vals2
     end
 end
