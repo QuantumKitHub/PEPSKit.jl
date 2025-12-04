c8a950bc8cdcaefa3a10954e71e3ece7f343c1e1
Lukas Devos <ldevos98@gmail.com>
2025-12-04 09:36:24 -0500

    make hermiticity an algorithm parameter

--- src/algorithms/bp/beliefpropagation.jl
+++ src/algorithms/bp/beliefpropagation.jl
@@ -2,6 +2,7 @@
     maxiter::Int = 50
     tol::Float64 = 1.0e-6
     verbosity::Int = 2
+    project_hermitian::Bool = true
 end
 
 """
@@ -16,7 +17,7 @@ function bp_fixedpoint(env::BPEnv, network::InfiniteSquareNetwork, alg::BeliefPr
         iter = 0
         while true
             iter += 1
-            env′ = bp_iteration(network, env)
+            env′ = bp_iteration(network, env, alg)
             ϵ = oftype(ϵ, tr_distance(env, env′))
             env = env′
 
@@ -37,10 +38,12 @@ end
 """
 One iteration to update the BP environment.
 """
-function bp_iteration(network::InfiniteSquareNetwork, env::BPEnv)
-    messages = similar(env.messages)
-    for I in CartesianIndices(messages)
-        messages[I] = normalize!(update_message(I, network, env))
+function bp_iteration(network::InfiniteSquareNetwork, env::BPEnv, alg::BeliefPropagation)
+    messages = map(eachindex(env)) do I
+        M = update_message(I, network, env)
+        normalize!(M)
+        alg.project_hermitian && (M = project_hermitian!!(t))
+        return M
     end
     return BPEnv(messages)
 end
@@ -51,12 +54,14 @@ Update the BP message in `env.messages[I]`.
 function update_message(I::CartesianIndex{3}, network::InfiniteSquareNetwork, env::BPEnv)
     dir, row, col = Tuple(I)
     (1 <= dir <= 4) || throw(ArgumentError("Invalid direction $dir"))
+
     A = network[row, col]
     dir == SOUTH || (M_north = env[NORTH, _prev(row, end), col])
     dir == WEST || (M_east = env[EAST, row, _next(col, end)])
     dir == NORTH || (M_south = env[SOUTH, _next(row, end), col])
     dir == EAST || (M_west = env[WEST, row, _prev(col, end)])
-    new_message = if dir == NORTH
+
+    return if dir == NORTH
         contract_north_message(A, M_west, M_north, M_east)
     elseif dir == EAST
         contract_east_message(A, M_north, M_east, M_south)
@@ -65,14 +70,6 @@ function update_message(I::CartesianIndex{3}, network::InfiniteSquareNetwork, en
     else # dir == WEST
         contract_west_message(A, M_south, M_west, M_north)
     end
-    @assert space(new_message) == space(env[I])
-    # TODO: enforce hermiticity (avoid accumulation of numerical errors)
-    if env[I] ≈ env[I]'
-        # ensure a single iteration preserves hermiticity
-        @assert new_message ≈ new_message'
-        new_message = (new_message + new_message') / 2
-    end
-    return new_message
 end
 
 function tr_distance(A::BPEnv, B::BPEnv)
@@ -87,3 +84,5 @@ end
 function trnorm!(M::AbstractTensorMap, p::Real = 1)
     return TensorKit._norm(svdvals!(M), p, zero(real(scalartype(M))))
 end
+
+project_hermitian!!(t) = add(t, t', 1 / 2, 1 / 2)
