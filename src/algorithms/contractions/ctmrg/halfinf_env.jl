3fa9074db828c3130f3ee8f5e1d0fc6a4a04bf81
Paul Brehmer <paul.brehmer@univie.ac.at>
2026-01-29 12:18:21 +0100

    Fix halfinf_env

--- src/algorithms/contractions/ctmrg/halfinf_env.jl
+++ src/algorithms/contractions/ctmrg/halfinf_env.jl
@@ -43,18 +43,18 @@ end
 function half_infinite_environment(
         C_1, C_2, E_1, E_2, E_3, E_4, A_1::P, A_2::P
     ) where {P <: PEPSSandwich}
-    return @autoopt @tensor env[χ_in D_inabove D_inbelow; χ_out D_outabove D_outbelow] :=
-        E_1[χ_in D1 D2; χ1] * C_1[χ1; χ2] * E_2[χ2 D3 D4; χ3] *
-        ket(A_1)[d1; D3 D9 D_inabove D1] * conj(bra(A_1)[d1; D4 D10 D_inbelow D2]) *
-        ket(A_2)[d2; D5 D7 D_outabove D9] * conj(bra(A_2)[d2; D6 D8 D_outbelow D10]) *
+    return @autoopt @tensor env[χ_out D_outabove D_outbelow; χ_in D_inabove D_inbelow] :=
+        E_1[χ_out D1 D2; χ1] * C_1[χ1; χ2] * E_2[χ2 D3 D4; χ3] *
+        ket(A_1)[d1; D3 D9 D_outabove D1] * conj(bra(A_1)[d1; D4 D10 D_outbelow D2]) *
+        ket(A_2)[d2; D5 D7 D_inabove D9] * conj(bra(A_2)[d2; D6 D8 D_inbelow D10]) *
         E_3[χ3 D5 D6; χ4] * C_2[χ4; χ5] * E_4[χ5 D7 D8; χ_out]
 end
 function half_infinite_environment(
         C_1, C_2, E_1, E_2, E_3, E_4, x::AbstractTensor{T, S, 3}, A_1::P, A_2::P
     ) where {T, S, P <: PEPSSandwich}
-    return @autoopt @tensor env_x[χ_in D_inabove D_inbelow] :=
-        E_1[χ_in D1 D2; χ1] * C_1[χ1; χ2] * E_2[χ2 D3 D4; χ3] *
-        ket(A_1)[d1; D3 D9 D_inabove D1] * conj(bra(A_1)[d1; D4 D10 D_inbelow D2]) *
+    return @autoopt @tensor env_x[χ_out D_outabove D_outbelow] :=
+        E_1[χ_out D1 D2; χ1] * C_1[χ1; χ2] * E_2[χ2 D3 D4; χ3] *
+        ket(A_1)[d1; D3 D9 D_outabove D1] * conj(bra(A_1)[d1; D4 D10 D_outbelow D2]) *
         ket(A_2)[d2; D5 D7 D11 D9] * conj(bra(A_2)[d2; D6 D8 D12 D10]) *
         E_3[χ3 D5 D6; χ4] * C_2[χ4; χ5] * E_4[χ5 D7 D8; χ6] *
         x[χ6 D11 D12]
@@ -72,18 +72,18 @@ end
 function half_infinite_environment(
         C_1, C_2, E_1, E_2, E_3, E_4, A_1::P, A_2::P
     ) where {P <: PFTensor}
-    return @autoopt @tensor env[χ_in D_in; χ_out D_out] :=
-        E_1[χ_in D1; χ1] * C_1[χ1; χ2] * E_2[χ2 D3; χ3] *
-        A_1[D1 D_in; D3 D9] *
-        A_2[D9 D_out; D5 D7] *
-        E_3[χ3 D5; χ4] * C_2[χ4; χ5] * E_4[χ5 D7; χ_out]
+    return @autoopt @tensor env[χ_out D_out; χ_in D_in] :=
+        E_1[χ_out D1; χ1] * C_1[χ1; χ2] * E_2[χ2 D3; χ3] *
+        A_1[D1 D_out; D3 D9] *
+        A_2[D9 D_in; D5 D7] *
+        E_3[χ3 D5; χ4] * C_2[χ4; χ5] * E_4[χ5 D7; χ_in]
 end
 function half_infinite_environment(
         C_1, C_2, E_1, E_2, E_3, E_4, x::AbstractTensor{T, S, 2}, A_1::P, A::P
     ) where {T, S, P <: PFTensor}
-    return @autoopt @tensor env_x[χ_in D_in] :=
-        E_1[χ_in D1; χ1] * C_1[χ1; χ2] * E_2[χ2 D3; χ3] *
-        A_1[D1 D_in; D3 D9] *
+    return @autoopt @tensor env_x[χ_out D_out] :=
+        E_1[χ_out D1; χ1] * C_1[χ1; χ2] * E_2[χ2 D3; χ3] *
+        A_1[D1 D_out; D3 D9] *
         A_2[D9 D11; D5 D7] *
         E_3[χ3 D5; χ4] * C_2[χ4; χ5] * E_4[χ5 D7; χ6] *
         x[χ6 D11]
