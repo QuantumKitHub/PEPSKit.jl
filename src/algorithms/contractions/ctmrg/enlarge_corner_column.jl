1b6c751e45772600136ca5f8ce3c5a43eda76728
Yue Zhengyuan <yuezy1997@icloud.com>
2026-02-05 20:31:53 +0800

    Make column enlarge corner work for any layers
    Co-authored-by: Lander Burgelman <39218680+leburgel@users.noreply.github.com>

--- src/algorithms/contractions/ctmrg/enlarge_corner_column.jl
+++ src/algorithms/contractions/ctmrg/enlarge_corner_column.jl
@@ -13,15 +13,12 @@ Contract the half-enlarged northwest corner of the CTMRG environment.
 ```
 """
 function column_enlarge_northwest_corner(
-        C_northwest::CTMRGCornerTensor, edge::CTMRG_PEPS_EdgeTensor
-    )
-    return @tensor corner[χS Dt Db; χE] := C_northwest[χS; χ] * edge[χ Dt Db; χE]
+        C_northwest::CTMRGCornerTensor, edge::CTMRGEdgeTensor
+    ) where {T, S}
+    pC = (codomainind(C_northwest), domainind(C_northwest))
+    pE = ((codomainind(edge)[1],), (codomainind(edge)[2:end]..., domainind(edge)...))
+    pCE = (codomainind(edge), domainind(edge))
+    return tensorcontract(C_northwest, pC, false, edge, pE, false, pCE)
 end
-function column_enlarge_northwest_corner(
-        C_northwest::CTMRGCornerTensor, edge::CTMRG_PF_EdgeTensor
-    )
-    return @tensor corner[χS D; χE] := C_northwest[χS; χ] * edge[χ D; χE]
-end
-# TODO: PEPS-PEPO-PEPS sandwich
 
 # TODO: Other column-enlarged corners when QR-CTMRG for arbitrary unit cell is possible
