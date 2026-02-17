00dc7c8580d8024cf76057c7e364bfd63e37c068
Paul Brehmer <paulbrehmer8@googlemail.com>
2026-02-17 17:58:30 +0100

    Fix gauge fixing index ordering

--- src/algorithms/optimization/fixed_point_differentiation.jl
+++ src/algorithms/optimization/fixed_point_differentiation.jl
@@ -294,6 +294,7 @@ end
 function gauge_fix(alg::SVDAdjoint, signs, info)
     # embed gauge signs in larger space to fix gauge of full U and V on truncated subspace
     rowsize, colsize = size(signs, 2), size(signs, 3)
+    inds = info.truncation_indices
     signs_full = map(Iterators.product(1:4, 1:rowsize, 1:colsize)) do (dir, r, c)
         σ = signs[dir, r, c]
         r_sign, c_sign = if dir == NORTH # take unit cell interdependency of signs into account
@@ -308,10 +309,9 @@ function gauge_fix(alg::SVDAdjoint, signs, info)
         extended_space = domain(info.U_full[dir, r_sign, c_sign]) ← codomain(info.V_full[dir, r_sign, c_sign])
         extended_σ = zeros(scalartype(σ), extended_space)
         for (c, b) in blocks(extended_σ)
-            σc = block(σ, c)
-            kept_dim = size(σc, 1)
-            b[diagind(b)] .= one(scalartype(σ)) # put ones on the diagonal
-            b[1:kept_dim, 1:kept_dim] .= σc # set to σ on kept subspace
+            I = get(inds, c, nothing)
+            @assert !isnothing(I)
+            block(extended_σ, c)[I, I] = b
         end
         return extended_σ
     end
@@ -320,7 +320,7 @@ function gauge_fix(alg::SVDAdjoint, signs, info)
     U_fixed, V_fixed = fix_relative_phases(info.U, info.V, signs)
     U_full_fixed, V_full_fixed = fix_relative_phases(info.U_full, info.V_full, signs_full)
     return SVDAdjoint(;
-        fwd_alg = FixedSVD(U_fixed, info.S, V_fixed, U_full_fixed, info.S_full, V_full_fixed),
+        fwd_alg = FixedSVD(U_fixed, info.S, V_fixed, U_full_fixed, info.S_full, V_full_fixed, inds),
         rrule_alg = alg.rrule_alg,
     )
 end
@@ -348,7 +348,7 @@ function gauge_fix(alg::EighAdjoint, signs, info)
     V_fixed = info.V * σ'
     V_full_fixed = info.V_full * extended_σ'
     return EighAdjoint(;
-        fwd_alg = FixedEig(info.D, V_fixed, info.D_full, V_full_fixed, info.truncation_indices),
+        fwd_alg = FixedEig(info.D, V_fixed, info.D_full, V_full_fixed, inds),
         rrule_alg = alg.rrule_alg,
     )
 end
