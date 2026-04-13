00685ef6e631de4fab041b1ae46ab27f1c2f1473
Paul Brehmer <paul.brehmer@univie.ac.at>
2026-04-13 09:23:51 +0200

    Fix `approximate` test

--- test/toolbox/approximate.jl
+++ test/toolbox/approximate.jl
@@ -3,19 +3,26 @@ using PEPSKit
 using Random
 using Test
 
+Vphys = ComplexSpace(2)
+Vbond = ComplexSpace(3)
+maxiter_approx = 8
+maxiter_boundary = 250
+sd = 123456
+
 @testset "approximate methods with FidelityMaxCrude" begin
-    peps1 = InfinitePEPS(2, 2)
-    peps2 = InfinitePEPS(2, 2)
-    Random.seed!(1234)
+    peps1 = InfinitePEPS(Vphys, Vbond)
+    peps2 = InfinitePEPS(Vphys, Vbond)
+    Random.seed!(sd)
     peps_approx1, = approximate(
-        peps1, peps2; maxiter = 3,
-        boundary_alg = (; trscheme = truncdim(6), verbosity = 1)
+        peps1, peps2;
+        maxiter = maxiter_approx,
+        boundary_alg = (; maxiter = maxiter_boundary, trunc = truncrank(6), verbosity = 1)
     )
-    Random.seed!(1234)
+    Random.seed!(sd) # reseeding with the same seed is required for the test to pass
     peps_approx2, = approximate(
         peps1, peps2, FidelityMaxCrude(;
-            maxiter = 3,
-            boundary_alg = (; trscheme = truncdim(6), verbosity = 1)
+            maxiter = maxiter_approx,
+            boundary_alg = (; maxiter = maxiter_boundary, trunc = truncrank(6), verbosity = 1)
         )
     )
 
