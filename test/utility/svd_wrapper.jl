ad73db5979b7683c591ed57377ac4fc007c9c3fc
leburgel <lander.burgelman@gmail.com>
2026-04-28 14:40:13 +0200

    Actually don't truncate through degeneracies by matching degeneracy with truncation space dimension

--- test/utility/svd_wrapper.jl
+++ test/utility/svd_wrapper.jl
@@ -74,7 +74,7 @@ end
 
 symm_m, symm_n = 18, 24
 symm_space = Z2Space(0 => symm_m, 1 => symm_n)
-symm_trspace = truncspace(Z2Space(0 => symm_m ÷ 2, 1 => symm_n ÷ 2))
+symm_trspace = truncspace(Z2Space(0 => symm_m ÷ 2, 1 => symm_n ÷ 3))
 symm_r = randn(dtype, symm_space, symm_space)
 symm_R = randn(dtype, space(symm_r))
 
@@ -112,7 +112,13 @@ end
 
 @testset "Truncated symmetric SVD broadening for $(alg.rrule_alg)" for alg in [full_alg, trunc_alg]
     u, s, v, = svd_compact(symm_r)
-    s.data[1:2:m] .= s.data[2:2:m] # make every singular value two-fold degenerate
+    # make every singular value in the 0-sector three-fold degenerate
+    b0 = diagview(block(s, Z2Irrep(0)))
+    b0[1:3:symm_m] .= b0[3:3:symm_m]
+    b0[2:3:symm_m] .= b0[3:3:symm_m]
+    # make every singular value in the 1-sector two-fold degenerate
+    b1 = diagview(block(s, Z2Irrep(1)))
+    b1[1:2:symm_n] .= b1[2:2:symm_n]
     symm_r_degen = u * s * v
 
     no_broadening_no_cutoff_alg = @set alg.rrule_alg.degeneracy_atol = 1.0e-30
