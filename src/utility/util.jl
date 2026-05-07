c8fc7f3359b48f14a4734fa6a9c83a0d9356b0c7
lkdvos <ldevos98@gmail.com>
2026-05-07 07:52:34 -0400

    Add periodic getindex/setindex! utility

--- src/utility/util.jl
+++ src/utility/util.jl
@@ -2,6 +2,52 @@
 _next(i, total) = mod1(i + 1, total)
 _prev(i, total) = mod1(i - 1, total)
 
+@inline _periodic_inds(data, J::NTuple{N, Int}) where {N} =
+    ntuple(i -> mod1(J[i], size(data, i)), Val(N))
+
+"""
+    periodic_getindex(A, data, I::Tuple)
+
+Periodic indexing of `A[I...]` backed by `data`.
+For `I::Vararg{Int}`, this yields `data[mod1.(I, size(data))...]`.
+Mixed index arguments (`Int` / `CartesianIndex`) are supported by flattening with `Base.to_indices`.
+
+!!! warning
+    Linear indexing schemes are not supported.
+"""
+@inline function periodic_getindex(A, data, I::Tuple)
+    J = to_indices(data, I)
+    return _periodic_getindex_dispatch(A, data, J)
+end
+
+# Dispatch helper
+_periodic_getindex_dispatch(A, data, J::Tuple{Vararg{Int}}) = @inbounds data[_periodic_inds(data, J)...]
+_periodic_getindex_dispatch(A, data, J::Tuple) = throw(ArgumentError("Invalid periodic indexing type $(typeof(J))"))
+
+"""
+    periodic_setindex!(A, data, v, I::Tuple)
+
+Periodic indexing assignment `A[I...] = v` backed by `data`, returning `v` so that the
+syntax `A[I...] = v` evaluates to `v` in line with Base Julia conventions.
+For `I::Vararg{Int}`, this yields `data[mod1.(I, size(data))...] = v`.
+Mixed index arguments (`Int` / `CartesianIndex`) are supported by flattening with `Base.to_indices`.
+
+!!! warning
+    Linear indexing schemes are not supported.
+"""
+@inline function periodic_setindex!(A, data, v, I::Tuple)
+    J = to_indices(data, I)
+    return _periodic_setindex_dispatch!(A, data, v, J)
+end
+
+# Dispatch helper
+function _periodic_setindex_dispatch!(A, data, v, J::Tuple{Vararg{Int}})
+    @inbounds data[_periodic_inds(data, J)...] = v
+    return v
+end
+_periodic_setindex_dispatch!(_A, data, v, J::Tuple) =
+    throw(ArgumentError("Invalid periodic indexing type $(typeof(J))"))
+
 # Get next and previous coordinate (direction, row, column), given a direction and going around the environment clockwise
 function _next_coordinate((dir, row, col), rowsize, colsize)
     if dir == 1
