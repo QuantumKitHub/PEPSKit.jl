34d1b311739e342a5cf5a21620ab6d64a1f914d7
Katharine Hyatt <kslimes@gmail.com>
2026-06-01 20:21:02 +0200

    Eigh pbs and tests

--- test/mooncake/eigh_wrapper.jl
+++ test/mooncake/eigh_wrapper.jl
@@ -1,8 +1,9 @@
+
 using Test
 using Random
 using LinearAlgebra
 using TensorKit
-using Mooncake
+using Mooncake 
 using Accessors
 using PEPSKit
 
@@ -11,29 +12,30 @@ using MatrixAlgebraKit: TruncatedAlgorithm, diagview
 # Gauge-invariant loss function
 function lossfun(A, alg, R = randn(space(A)), trunc = notrunc())
     alg = @set alg.fwd_alg = TruncatedAlgorithm(alg.fwd_alg, trunc)
-    U, S, V, = svd_trunc(A, alg)
-    return real(dot(R, U * V)) + dot(S, S)  # Overlap with random tensor R is gauge-invariant and differentiable, also for m≠n
+    D, V, = eigh_trunc(project_hermitian(A), alg)
+    return real(dot(R, V * V')) + dot(D, D)  # Overlap with random tensor R is gauge-invariant and differentiable
 end
 
-
 dtype = ComplexF64
-m, n = 20, 30
-χ = 12
+n = 20
+χ = 10
 trunc = truncspace(ℂ^χ)
 rtol = 1.0e-9
-Random.seed!(12345678)
-r = randn(dtype, ℂ^m, ℂ^n)
+Random.seed!(123456789)
+r = randn(dtype, ℂ^n, ℂ^n)
+r = 0.5 * (r + r') # make r Hermitian
 R = randn(space(r))
+R = 0.5 * (R + R')
 
-full_alg = SVDAdjoint(; rrule_alg = (; alg = :FullPullback, degeneracy_atol = 1.0e-13))
-trunc_alg = SVDAdjoint(; rrule_alg = (; alg = :TruncPullback, degeneracy_atol = 1.0e-13))
-iter_alg = SVDAdjoint(; fwd_alg = (; alg = :GKL))
+full_alg = EighAdjoint(; fwd_alg = (; alg = :QRIteration), rrule_alg = (; alg = :FullPullback))
+trunc_alg = EighAdjoint(; fwd_alg = (; alg = :QRIteration), rrule_alg = (; alg = :TruncPullback))
+iter_alg = EighAdjoint(; fwd_alg = (; alg = :Lanczos), rrule_alg = (; alg = :TruncPullback))
 
-@testset "Non-truncated SVD" begin
+@testset "Non-truncated eigh" begin
     full_lossfun = A -> lossfun(A, full_alg, R)
     trunc_lossfun = A -> lossfun(A, trunc_alg, R)
     iter_lossfun = A -> lossfun(A, iter_alg, R)
-
+    
     full_rrule = Mooncake.build_rrule(full_lossfun, r)
     trunc_rrule = Mooncake.build_rrule(trunc_lossfun, r)
     iter_rrule = Mooncake.build_rrule(iter_lossfun, r)
@@ -48,7 +50,7 @@ iter_alg = SVDAdjoint(; fwd_alg = (; alg = :GKL))
     @test g_trunc[2] ≈ g_iter[2] rtol = rtol
 end
 
-@testset "Truncated SVD with χ=$χ" begin
+@testset "Truncated eigh with χ=$χ" begin
     full_lossfun = A -> lossfun(A, full_alg, R, trunc)
     trunc_lossfun = A -> lossfun(A, trunc_alg, R, trunc)
     iter_lossfun = A -> lossfun(A, iter_alg, R, trunc)
@@ -67,13 +69,13 @@ end
     @test g_trunc[2] ≈ g_iter[2] rtol = rtol
 end
 
-@testset "Truncated SVD broadening for $(alg.rrule_alg)" for alg in [full_alg, trunc_alg]
-    u, s, v, = svd_compact(r)
-    s.data[1:2:m] .= s.data[2:2:m] # make every singular value two-fold degenerate
-    r_degen = u * s * v
+@testset "Truncated eigh broadening for $(alg.rrule_alg)" for alg in [full_alg, trunc_alg]
+    d, v = eigh_full(r)
+    d.data[1:2:n] .= d.data[2:2:n] # make every eigenvalue two-fold degenerate
+    r_degen = v * d * v'
 
-    no_broadening_no_cutoff_alg = @set full_alg.rrule_alg.degeneracy_atol = 1.0e-30
-    small_broadening_alg = @set full_alg.rrule_alg.degeneracy_atol = 1.0e-13
+    no_broadening_no_cutoff_alg = @set alg.rrule_alg.degeneracy_atol = 1.0e-30
+    small_broadening_alg = @set alg.rrule_alg.degeneracy_atol = 1.0e-13
 
     only_lossfun = A -> lossfun(A, alg, R, trunc)
     no_broadening_lossfun = A -> lossfun(A, no_broadening_no_cutoff_alg, R, trunc)
@@ -100,9 +102,11 @@ symm_m, symm_n = 18, 24
 symm_space = Z2Space(0 => symm_m, 1 => symm_n)
 symm_trspace = truncspace(Z2Space(0 => symm_m ÷ 2, 1 => symm_n ÷ 3))
 symm_r = randn(dtype, symm_space, symm_space)
+symm_r = 0.5 * (symm_r + symm_r')
 symm_R = randn(dtype, space(symm_r))
+symm_R = 0.5 * (symm_R + symm_R')
 
-@testset "IterSVD of symmetric tensors" begin
+@testset "IterEig of symmetric tensors" begin
     full_lossfun = A -> lossfun(A, full_alg, symm_R)
     trunc_lossfun = A -> lossfun(A, trunc_alg, symm_R)
     iter_lossfun = A -> lossfun(A, iter_alg, symm_R)
@@ -119,7 +123,7 @@ symm_R = randn(dtype, space(symm_r))
     @test g_full[2] ≈ g_trunc[2] rtol = rtol
     @test g_full[2] ≈ g_iter[2] rtol = rtol
     @test g_trunc[2] ≈ g_iter[2] rtol = rtol
-    
+
     full_lossfun = A -> lossfun(A, full_alg, symm_R, symm_trspace)
     trunc_lossfun = A -> lossfun(A, trunc_alg, symm_R, symm_trspace)
     iter_lossfun = A -> lossfun(A, iter_alg, symm_R, symm_trspace)
@@ -137,7 +141,6 @@ symm_R = randn(dtype, space(symm_r))
     @test g_trunc_tr[2] ≈ g_iter_tr[2] rtol = rtol
 
     iter_alg_fallback = @set iter_alg.fwd_alg.fallback_threshold = 0.4  # Do dense decomposition in one block, sparse one in the other
-    
     fb_lossfun = A -> lossfun(A, iter_alg_fallback, symm_R, symm_trspace)
     fb_rrule = Mooncake.build_rrule(fb_lossfun, symm_r)
     l_iter_fb, g_iter_fb = Mooncake.value_and_gradient!!(fb_rrule, fb_lossfun, symm_r)
@@ -146,28 +149,28 @@ symm_R = randn(dtype, space(symm_r))
     @test g_trunc_tr[2] ≈ g_iter_fb[2] rtol = rtol
 end
 #=
-@testset "Truncated symmetric SVD broadening for $(alg.rrule_alg)" for alg in [full_alg, trunc_alg]
-    u, s, v, = svd_compact(symm_r)
+@testset "Truncated symmetric eigh broadening for $(alg.rrule_alg)" for alg in [full_alg, trunc_alg]
+    d, v = eigh_full(symm_r)
     # make every singular value in the 0-sector three-fold degenerate
-    b0 = diagview(block(s, Z2Irrep(0)))
+    b0 = diagview(block(d, Z2Irrep(0)))
     b0[1:3:symm_m] .= b0[3:3:symm_m]
     b0[2:3:symm_m] .= b0[3:3:symm_m]
     # make every singular value in the 1-sector two-fold degenerate
-    b1 = diagview(block(s, Z2Irrep(1)))
+    b1 = diagview(block(d, Z2Irrep(1)))
     b1[1:2:symm_n] .= b1[2:2:symm_n]
-    symm_r_degen = u * s * v
+    symm_r_degen = v * d * v'
 
     no_broadening_no_cutoff_alg = @set alg.rrule_alg.degeneracy_atol = 1.0e-30
     small_broadening_alg = @set alg.rrule_alg.degeneracy_atol = 1.0e-13
 
-    l_only_cutoff, g_only_cutoff = Mooncake.value_and_gradient!!(
+    l_only_cutoff, g_only_cutoff = withgradient(
         A -> lossfun(A, alg, symm_R, symm_trspace), symm_r_degen
     ) # cutoff sets degenerate difference to zero
-    l_no_broadening_no_cutoff, g_no_broadening_no_cutoff = Mooncake.value_and_gradient!!( # degenerate singular value differences lead to divergent contributions
+    l_no_broadening_no_cutoff, g_no_broadening_no_cutoff = withgradient( # degenerate singular value differences lead to divergent contributions
         A -> lossfun(A, no_broadening_no_cutoff_alg, symm_R, symm_trspace),
         symm_r_degen,
     )
-    l_small_broadening, g_small_broadening = Mooncake.value_and_gradient!!( # broadening smoothens divergent contributions
+    l_small_broadening, g_small_broadening = withgradient( # broadening smoothens divergent contributions
         A -> lossfun(A, small_broadening_alg, symm_R, symm_trspace),
         symm_r_degen,
     )
@@ -175,49 +178,4 @@ end
     @test l_only_cutoff ≈ l_no_broadening_no_cutoff ≈ l_small_broadening
     @test norm(g_no_broadening_no_cutoff[1] - g_small_broadening[1]) > 1.0e-2 # divergences mess up the gradient
     @test g_only_cutoff[1] ≈ g_small_broadening[1] rtol = rtol # cutoff and broadening have similar effect
-end
-=#
-# TODO: Add when IterSVD is implemented for HalfInfiniteEnv
-# χbond = 2
-# χenv = 6
-# ctm_alg = CTMRG(; tol=1e-10, verbosity=2, svd_alg=SVDAdjoint())
-# Random.seed!(91283219347)
-# H = heisenberg_XYZ(InfiniteSquare())
-# psi = InfinitePEPS(ComplexSpace(2), ComplexSpace(χbond))
-# env = leading_boundary(CTMRGEnv(psi, ComplexSpace(χenv)), psi, ctm_alg);
-# hienv = HalfInfiniteEnv(
-#     env.corners[1],
-#     env.corners[2],
-#     env.edges[4],
-#     env.edges[1],
-#     env.edges[1],
-#     env.edges[2],
-#     psi[1],
-#     psi[1],
-#     psi[1],
-#     psi[1],
-# )
-# hienv_dense = hienv()
-# env_R = randn(space(hienv))
-
-# svd_trunc!(hienv, iter_alg)
-
-# @testset "IterSVD with HalfInfiniteEnv function handle" begin
-#     # Equivalence of dense and sparse contractions
-#     x₀ = PEPSKit.random_start_vector(hienv)
-#     x′ = hienv(x₀, Val(false))
-#     x″ = hienv(x′, Val(true))
-#     x‴ = hienv(x″, Val(false))
-
-#     a = hienv_dense * x₀
-#     b = hienv_dense' * a
-#     c = hienv_dense * b
-#     @test a ≈ x′
-#     @test b ≈ x″
-#     @test c ≈ x‴
-
-#     # l_fullsvd, g_fullsvd = withgradient(A -> lossfun(A, full_alg, env_R), hienv_dense)
-#     # l_itersvd, g_itersvd = withgradient(A -> lossfun(A, iter_alg, env_R), hienv)
-#     # @test l_itersvd ≈ l_fullsvd
-#     # @test g_fullsvd[1] ≈ g_itersvd[1] rtol = rtol
-# end
+end=#
