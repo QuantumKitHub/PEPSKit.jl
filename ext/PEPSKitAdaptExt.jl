6ab850aee9749ab4ce464bed7f49a55ba6c3ffad
Katharine Hyatt <katharine.hyatt@ugent.be>
2026-07-29 17:44:11 +0200

    Add a simple timestep example

--- ext/PEPSKitAdaptExt.jl
+++ ext/PEPSKitAdaptExt.jl
@@ -8,4 +8,9 @@ function Adapt.adapt_structure(to, x::PEPSKit.LocalOperator{T, S}) where {T, S}
     return PEPSKit.LocalOperator{valtype(terms′)}(x.lattice, terms′)
 end
 
+function Adapt.adapt_structure(to, x::PEPSKit.InfinitePEPS{T}) where {T}
+    A′ = map(a -> adapt(to, a), x.A)
+    return InfinitePEPS{eltype(A′)}(A′)
+end
+
 end
