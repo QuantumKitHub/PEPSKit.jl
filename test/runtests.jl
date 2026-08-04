a781aabd7f2d0494703f70fe1cb5a4cac2b98323
Katharine Hyatt <kslimes@gmail.com>
2026-08-04 12:33:50 +0200

    Don't run all tests on GPU

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using ParallelTestRunner
 using PEPSKit
 
+testsuite = ParallelTestRunner.find_tests(@__DIR__)
+
+# CUDA tests: only run if CUDA is functional
+using CUDA: CUDA
+CUDA.functional() || filter!(!startswith("cuda") ∘ first, testsuite)
+# AMDGPU tests: only run if AMDGPU is functional
+using AMDGPU
+AMDGPU.functional() || filter!(!startswith("amd") ∘ first, testsuite)
+
+# On Buildkite (GPU CI runner): only run CUDA and AMDGPU tests
+if get(ENV, "BUILDKITE", "false") == "true"
+    f(str) = startswith(first(str), "cuda") || startswith(first(str), "amd")
+    filter!(f, testsuite)
+end
+
 # --fast to indicate a smaller set of tests
 args = parse_args(ARGS; custom = ["fast"])
 fast = !isnothing(args.custom["fast"])
@@ -9,4 +24,4 @@ const init_code = quote
     const fast_tests = $fast
 end
 
-ParallelTestRunner.runtests(PEPSKit, args; init_code)
+ParallelTestRunner.runtests(PEPSKit, args; testsuite, init_code)
