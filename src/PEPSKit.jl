661eff079d02473fbb0b3df16fc34f924a8a1e1c
Katharine Hyatt <katharine.hyatt@ugent.be>
2026-08-19 12:26:15 +0200

    Use alloc_caches from GPUArrays

--- src/PEPSKit.jl
+++ src/PEPSKit.jl
@@ -55,6 +55,7 @@ using DocStringExtensions
 include("Defaults.jl")  # Include first to allow for docstring interpolation with Defaults values
 
 include("utility/util.jl")
+include("utility/alloc_cache.jl")
 include("utility/indexing.jl")
 include("utility/diffable_threads.jl")
 include("utility/twistdual.jl")
