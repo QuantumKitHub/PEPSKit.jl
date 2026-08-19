894d8e9f6711a719c1cb41ac138d9fcb11896c88
Katharine Hyatt <katharine.hyatt@ugent.be>
2026-08-19 22:12:30 +0200

    Let the cache depth vary by algo

--- ext/PEPSKitGPUArraysExt.jl
+++ ext/PEPSKitGPUArraysExt.jl
@@ -4,22 +4,23 @@ using GPUArrays
 using GPUArrays: AnyGPUArray, AllocCache
 using PEPSKit
 
-# Each caller (such as `su_iter`) gets a pair of caches. This makes sense to do on a per-caller basis 
+# Each caller (such as `su_iter`) gets a pair of caches. This makes sense to do on a per-caller basis
 # because what is being cached varies between algorithms.
-# For each caller we also store *two* caches, one for even iterations and one for odd.
+# For each caller we also store several caches, for SimultaneousCTMRG and SU,
+# one for even iterations and one for odd, and for SequentialCTMRG, 5 (one "round" plus one extra)
 # This has to be done because we can't reuse a cache from iteration `i`
-# until iteration `i+1` is completely finished and its result handed off to iteration `i+2`. 
-const ALLOC_CACHES = Dict{Symbol, NTuple{2, AllocCache}}()
+# until iteration `i+n` is completely finished and its result handed off.
+const ALLOC_CACHES = Dict{Tuple{Symbol, Int}, Vector{AllocCache}}()
 const ALLOC_CACHES_LOCK = ReentrantLock()
 
-function _caches(site::Symbol)
+function _caches(site::Symbol, depth::Int)
     return Base.@lock ALLOC_CACHES_LOCK begin
-        get!(() -> (AllocCache(), AllocCache()), ALLOC_CACHES, site)
+        get!(() -> [AllocCache() for _ in 1:depth], ALLOC_CACHES, (site, depth))
     end
 end
 
-function PEPSKit._with_alloc_cache(f, ::Type{<:AnyGPUArray}, site::Symbol, iter::Int)
-    cache = @inbounds _caches(site)[mod1(iter + 1, 2)]
+function PEPSKit._with_alloc_cache(f, ::Type{<:AnyGPUArray}, site::Symbol, iter::Int, depth::Int)
+    cache = @inbounds _caches(site, depth)[mod1(iter + 1, depth)]
     return GPUArrays.@cached cache f()
 end
 
