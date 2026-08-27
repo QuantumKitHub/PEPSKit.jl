35788f4164d71531ea892c018e0093392804d9de
leburgel <lander.burgelman@gmail.com>
2026-08-27 14:44:57 +0200

    Evaluate `BPEnv` expectation values in exactly the same way, and add reduced density operators

--- src/algorithms/contractions/bp_contractions.jl
+++ src/algorithms/contractions/bp_contractions.jl
@@ -48,36 +48,110 @@ absorb_south_message(A::PEPSTensor, M::PEPSMessage) =
 absorb_west_message(A::PEPSTensor, M::PEPSMessage) =
     @tensor A′[d; N E S W'] := A[d; N E S W] * M[W'; W]
 
-# Belief Propagation Expectation values
-# -------------------------------------
-function MPSKit.expectation_value(peps::InfinitePEPS, O::LocalOperator, env::BPEnv)
-    checklattice(peps, O)
-    term_vals = dtmap([O.terms...]) do (inds, operator)  # OhMyThreads can't iterate over O.terms directly
-        contract_local_operator(inds, operator, peps, peps, env) /
-            contract_local_norm(inds, peps, peps, env)
-    end
-    return sum(term_vals)
-end
-
-function contract_local_operator(
+# Belief Propagation reduced density matrices
+# -------------------------------------------
+# BP messages live on the bonds of the network, so there is no analogue of the generated
+# rectangular-patch contraction used for a `CTMRGEnv`: only single sites and nearest
+# neighbor pairs can be contracted without introducing loop corrections.
+function reduced_densitymatrix(
         inds::Vector{CartesianIndex{2}},
-        O::AbstractTensorMap,
         ket::InfinitePEPS,
         bra::InfinitePEPS,
         env::BPEnv,
     )
-    length(inds) == 1 && return contract_local_operator1x1(only(inds), O, ket, bra, env)
+    length(inds) == 1 && return reduced_densitymatrix1x1(only(inds), ket, bra, env)
 
     if length(inds) == 2
         ind_relative = inds[2] - inds[1]
         if ind_relative == CartesianIndex(1, 0)
-            return contract_local_operator2x1(inds[1], O, ket, bra, env)
+            return reduced_densitymatrix2x1(inds[1], ket, bra, env)
         elseif ind_relative == CartesianIndex(0, 1)
-            return contract_local_operator1x2(inds[1], O, ket, bra, env)
+            return reduced_densitymatrix1x2(inds[1], ket, bra, env)
         end
     end
     error("No implementation for contractions for BP environments with $inds")
 end
+reduced_densitymatrix(inds, ket::InfinitePEPS, env::BPEnv) =
+    reduced_densitymatrix(inds, ket, ket, env)
+
+function reduced_densitymatrix1x1(
+        ind::CartesianIndex{2}, ket::InfinitePEPS, bra::InfinitePEPS, env::BPEnv
+    )
+    row, col = Tuple(ind)
+    M_north = env[NORTH, row - 1, col]
+    M_east = env[EAST, row, col + 1]
+    M_south = env[SOUTH, row + 1, col]
+    M_west = env[WEST, row, col - 1]
+
+    @autoopt @tensor ρ[dt; db] :=
+        ket[row, col][dt; DNt DEt DSt DWt] *
+        conj(bra[row, col][db; DNb DEb DSb DWb]) *
+        M_north[DNt; DNb] *
+        M_east[DEt; DEb] *
+        M_south[DSb; DSt] *
+        M_west[DWb; DWt]
+
+    return ρ / str(ρ)
+end
+
+function reduced_densitymatrix2x1(
+        coord::CartesianIndex{2}, ket::InfinitePEPS, bra::InfinitePEPS, env::BPEnv
+    )
+    row, col = Tuple(coord)
+    M_north = env[NORTH, row - 1, col]
+    M_northeast = env[EAST, row, col + 1]
+    M_southeast = env[EAST, row + 1, col + 1]
+    M_south = env[SOUTH, row + 2, col]
+    M_southwest = env[WEST, row + 1, col - 1]
+    M_northwest = env[WEST, row, col - 1]
+
+    @autoopt @tensor ρ[dNt dSt; dNb dSb] :=
+        ket[row, col][dNt; DNt DNEt DMt DNWt] *
+        ket[row + 1, col][dSt; DMt DSEt DSt DSWt] *
+        conj(bra[row, col][dNb; DNb DNEb DMb DNWb]) *
+        conj(bra[row + 1, col][dSb; DMb DSEb DSb DSWb]) *
+        M_north[DNt; DNb] *
+        M_northeast[DNEt; DNEb] *
+        M_southeast[DSEt; DSEb] *
+        M_south[DSb; DSt] *
+        M_southwest[DSWb; DSWt] *
+        M_northwest[DNWb; DNWt]
+
+    return ρ / str(ρ)
+end
+
+function reduced_densitymatrix1x2(
+        coord::CartesianIndex{2}, ket::InfinitePEPS, bra::InfinitePEPS, env::BPEnv
+    )
+    row, col = Tuple(coord)
+    M_west = env[WEST, row, col - 1]
+    M_northwest = env[NORTH, row - 1, col]
+    M_northeast = env[NORTH, row - 1, col + 1]
+    M_east = env[EAST, row, col + 2]
+    M_southeast = env[SOUTH, row + 1, col + 1]
+    M_southwest = env[SOUTH, row + 1, col]
+    A_west = ket[row, col]
+    Ā_west = bra[row, col]
+    A_east = ket[row, col + 1]
+    Ā_east = bra[row, col + 1]
+
+    @autoopt @tensor ρ[dWt dEt; dWb dEb] :=
+        A_west[dWt; DNWt DMt DSWt DWt] *
+        A_east[dEt; DNEt DEt DSEt DMt] *
+        conj(Ā_west[dWb; DNWb DMb DSWb DWb]) *
+        conj(Ā_east[dEb; DNEb DEb DSEb DMb]) *
+        M_west[DWb; DWt] *
+        M_northwest[DNWt; DNWb] *
+        M_northeast[DNEt; DNEb] *
+        M_east[DEt; DEb] *
+        M_southeast[DSEb; DSEt] *
+        M_southwest[DSWb; DSWt]
+
+    return ρ / str(ρ)
+end
+
+# Local norm contractions
+# ----------------------
 function contract_local_norm(
         inds::Vector{CartesianIndex{2}},
         ket::InfinitePEPS,
@@ -97,30 +171,6 @@ function contract_local_norm(
     error("No implementation for contractions for BP environments with $inds")
 end
 
-function contract_local_operator1x1(
-        ind::CartesianIndex{2},
-        O::AbstractTensorMap{<:Any, <:Any, 1, 1},
-        ket::InfinitePEPS,
-        bra::InfinitePEPS,
-        env::BPEnv,
-    )
-    row, col = Tuple(ind)
-    M_north = env[NORTH, row - 1, col]
-    M_east = env[EAST, row, col + 1]
-    M_south = env[SOUTH, row + 1, col]
-    M_west = env[WEST, row, col - 1]
-
-    return @autoopt @tensor begin
-        ket[row, col][dt; DNt DEt DSt DWt] *
-            conj(bra[row, col][db; DNb DEb DSb DWb]) *
-            O[db; dt] *
-            M_north[DNt; DNb] *
-            M_east[DEt; DEb] *
-            M_south[DSb; DSt] *
-            M_west[DWb; DWt]
-    end
-end
-
 function contract_local_norm1x1(
         ind::CartesianIndex{2}, ket::InfinitePEPS, bra::InfinitePEPS, env::BPEnv
     )
@@ -137,68 +187,6 @@ function contract_local_norm1x1(
             M_east[DEt; DEb] *
             M_south[DSb; DSt] *
             M_west[DWb; DWt]
-    end
-end
-
-function contract_local_operator2x1(
-        coord::CartesianIndex{2},
-        O::AbstractTensorMap{<:Any, <:Any, 2, 2},
-        ket::InfinitePEPS,
-        bra::InfinitePEPS,
-        env::BPEnv,
-    )
-    row, col = Tuple(coord)
-    M_north = env[NORTH, row - 1, col]
-    M_northeast = env[EAST, row, col + 1]
-    M_southeast = env[EAST, row + 1, col + 1]
-    M_south = env[SOUTH, row + 2, col]
-    M_southwest = env[WEST, row + 1, col - 1]
-    M_northwest = env[WEST, row, col - 1]
-
-    return @autoopt @tensor ket[row, col][dNt; DNt DNEt DMt DNWt] *
-        ket[row + 1, col][dSt; DMt DSEt DSt DSWt] *
-        conj(bra[row, col][dNb; DNb DNEb DMb DNWb]) *
-        conj(bra[row + 1, col][dSb; DMb DSEb DSb DSWb]) *
-        M_north[DNt; DNb] *
-        M_northeast[DNEt; DNEb] *
-        M_southeast[DSEt; DSEb] *
-        M_south[DSb; DSt] *
-        M_southwest[DSWb; DSWt] *
-        M_northwest[DNWb; DNWt] *
-        O[dNb dSb; dNt dSt]
-end
-
-function contract_local_operator1x2(
-        coord::CartesianIndex{2},
-        O::AbstractTensorMap{<:Any, <:Any, 2, 2},
-        ket::InfinitePEPS,
-        bra::InfinitePEPS,
-        env::BPEnv,
-    )
-    row, col = Tuple(coord)
-    M_west = env[WEST, row, col - 1]
-    M_northwest = env[NORTH, row - 1, col]
-    M_northeast = env[NORTH, row - 1, col + 1]
-    M_east = env[EAST, row, col + 2]
-    M_southeast = env[SOUTH, row + 1, col + 1]
-    M_southwest = env[SOUTH, row + 1, col]
-    A_west = ket[row, col]
-    Ā_west = bra[row, col]
-    A_east = ket[row, col + 1]
-    Ā_east = bra[row, col + 1]
-
-    return @autoopt @tensor begin
-        A_west[dWt; DNWt DMt DSWt DWt] *
-            A_east[dEt; DNEt DEt DSEt DMt] *
-            conj(Ā_west[dWb; DNWb DMb DSWb DWb]) *
-            conj(Ā_east[dEb; DNEb DEb DSEb DMb]) *
-            M_west[DWb; DWt] *
-            M_northwest[DNWt; DNWb] *
-            M_northeast[DNEt; DNEb] *
-            M_east[DEt; DEb] *
-            M_southeast[DSEb; DSEt] *
-            M_southwest[DSWb; DSWt] *
-            O[dWb dEb; dWt dEt]
     end
 end
 
