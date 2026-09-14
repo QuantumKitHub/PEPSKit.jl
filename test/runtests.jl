72a162e6d742dcba6757063c7a4161dadedc0c9a
Katharine Hyatt <kslimes@gmail.com>
2026-09-14 16:25:40 +0200

    Move tests into testsuite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,11 @@
 using ParallelTestRunner
 using PEPSKit
 
-testsuite = ParallelTestRunner.find_tests(@__DIR__)
+# Start with autodiscovered tests
+testsuite = find_tests(@__DIR__)
 
-# On Buildkite (GPU CI runner): only run CUDA and AMDGPU tests
-if get(ENV, "BUILDKITE", "false") == "true"
-    f(str) = startswith(first(str), "cuda") || startswith(first(str), "amd")
-    filter!(f, testsuite)
-end
+# remove testsuite
+filter!(!(startswith("testsuite") ∘ first), testsuite)
 
 # --fast to indicate a smaller set of tests
 args = parse_args(ARGS; custom = ["fast"])
