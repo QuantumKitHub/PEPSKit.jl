2a67cf7a9878736463fd37856c10e31d3b3459be
Yue Zhengyuan <yuezy1997@icloud.com>
2026-09-15 09:23:00 +0800

    Separate `mpo_path` functions from their generated contraction part

--- src/algorithms/contractions/mpo_path/pepo_1layer.jl
+++ src/algorithms/contractions/mpo_path/pepo_1layer.jl
@@ -40,7 +40,11 @@ end
 Return the tensor-expression label for the PEPO virtual leg in a cardinal direction.
 """
 function _mpo_path_virtual_label(direction::Symbol)
-    return (:N, :E, :S, :W)[_mpo_path_direction(direction)]
+    direction === :north && return :N
+    direction === :east && return :E
+    direction === :south && return :S
+    direction === :west && return :W
+    throw(ArgumentError("invalid MPO path direction: $direction"))
 end
 
 """
@@ -83,77 +87,96 @@ function _mpo_path_result_expr(directions)
 end
 
 """
-Act the first tensor `op` of an OBC-MPO on PEPO tensor `A` and fuse the
-outgoing MPO string with the virtual space of `A` along `direction`.
+Act the first tensor `op` of an OBC-MPO on PEPO tensor `A` and fuse the outgoing MPO string with the virtual space of `A` along `direction`.
 """
-@generated function mpo_path_first(A::PEPOTensor, op, ::Val{direction}) where {direction}
-    direction_index = _mpo_path_direction(direction)
+function mpo_path_first(A::PEPOTensor, op, direction::Val{D}) where {D}
+    _check_pepo_first_physicalspace(A, op)
+    direction_index = _mpo_path_direction(D)
+    A′ = twistdual(A, 2)
+    F = _mpo_path_outgoing_fuser(
+        fuser(storagetype(A), domain(A, direction_index)', space(op, 3)),
+        direction_index,
+    )
+    return _mpo_path_first(A′, op, F, direction)
+end
+
+"""
+Contract a prepared PEPO tensor with the first MPO tensor and its outgoing fuser.
+"""
+@generated function _mpo_path_first(A, op, F, ::Val{direction}) where {direction}
     virtual_label = _mpo_path_virtual_label(direction)
     fused_label = Symbol(virtual_label, :r)
 
     result_e = _mpo_path_result_expr(((direction, :r),))
     op_e = tensorexpr(:op, :dout, (:din, :r))
-    A_e = tensorexpr(:A′, (:din, :dout), (:N, :E, :S, :W))
+    A_e = tensorexpr(:A, (:din, :dout), (:N, :E, :S, :W))
     F_e = tensorexpr(:F, fused_label, (virtual_label, :r))
     rhs = Expr(:call, :*, op_e, A_e, F_e)
-    contraction = macroexpand(
+    return macroexpand(
         @__MODULE__, :(return @tensoropt $result_e := $rhs)
     )
-
-    return quote
-        _check_pepo_first_physicalspace(A, op)
-        A′ = twistdual(A, 2)
-        F = _mpo_path_outgoing_fuser(
-            fuser(storagetype(A), domain(A, $direction_index)', space(op, 3)),
-            $direction_index,
-        )
-        $contraction
-    end
 end
 
 """
-Act the last tensor `op` of an OBC-MPO on PEPO tensor `A` and fuse the
-incoming MPO string with the virtual space of `A` along `direction`.
+Act the last tensor `op` of an OBC-MPO on PEPO tensor `A` and fuse the incoming MPO string with the virtual space of `A` along `direction`.
 """
-@generated function mpo_path_last(A::PEPOTensor, op, ::Val{direction}) where {direction}
-    direction_index = _mpo_path_direction(direction)
+function mpo_path_last(A::PEPOTensor, op, direction::Val{D}) where {D}
+    _check_pepo_last_physicalspace(A, op)
+    direction_index = _mpo_path_direction(D)
+    A′ = twistdual(A, 2)
+    F = _mpo_path_incoming_fuser(
+        fuser(storagetype(A), domain(A, direction_index), space(op, 1)'),
+        direction_index,
+    )
+    return _mpo_path_last(A′, op, F, direction)
+end
+
+"""
+Contract a prepared PEPO tensor with the last MPO tensor and its incoming fuser.
+"""
+@generated function _mpo_path_last(A, op, F, ::Val{direction}) where {direction}
     virtual_label = _mpo_path_virtual_label(direction)
     fused_label = Symbol(virtual_label, :l)
 
     result_e = _mpo_path_result_expr(((direction, :l),))
     F_e = Expr(:call, :conj, tensorexpr(:F, fused_label, (virtual_label, :l)))
     op_e = tensorexpr(:op, (:l, :dout), :din)
-    A_e = tensorexpr(:A′, (:din, :dout), (:N, :E, :S, :W))
+    A_e = tensorexpr(:A, (:din, :dout), (:N, :E, :S, :W))
     rhs = Expr(:call, :*, F_e, op_e, A_e)
-    contraction = macroexpand(
+    return macroexpand(
         @__MODULE__, :(return @tensoropt $result_e := $rhs)
     )
-
-    return quote
-        _check_pepo_last_physicalspace(A, op)
-        A′ = twistdual(A, 2)
-        F = _mpo_path_incoming_fuser(
-            fuser(storagetype(A), domain(A, $direction_index), space(op, 1)'),
-            $direction_index,
-        )
-        $contraction
-    end
 end
 
 """
-Act the middle tensor `op` of an MPO on PEPO tensor `A` and fuse the
-incoming and the outgoing MPO string with the virtual space of `A` along
-`directions = (incoming, outgoing)`.
+Act the middle tensor `op` of an MPO on PEPO tensor `A` and fuse the incoming and outgoing MPO strings with the virtual space of `A` along `directions = (incoming, outgoing)`.
 """
-@generated function mpo_path_middle(
-        A::PEPOTensor, op, ::Val{directions}
-    ) where {directions}
-    incoming, outgoing = directions
+function mpo_path_middle(A::PEPOTensor, op, directions::Val{D}) where {D}
+    incoming, outgoing = D
     incoming == outgoing &&
         throw(ArgumentError("MPO path should enter and exit in different directions"))
-
+    _check_pepo_middle_physicalspace(A, op)
     incoming_index = _mpo_path_direction(incoming)
     outgoing_index = _mpo_path_direction(outgoing)
+    A′ = twistdual(A, 2)
+    Fin = _mpo_path_incoming_fuser(
+        fuser(storagetype(A), domain(A, incoming_index), space(op, 1)'),
+        incoming_index,
+    )
+    Fout = _mpo_path_outgoing_fuser(
+        fuser(storagetype(A), domain(A, outgoing_index)', space(op, 4)),
+        outgoing_index,
+    )
+    return _mpo_path_middle(A′, op, Fin, Fout, directions)
+end
+
+"""
+Contract a prepared PEPO tensor with a middle MPO tensor and its incoming and outgoing fusers.
+"""
+@generated function _mpo_path_middle(
+        A, op, Fin, Fout, ::Val{directions}
+    ) where {directions}
+    incoming, outgoing = directions
     incoming_label = _mpo_path_virtual_label(incoming)
     outgoing_label = _mpo_path_virtual_label(outgoing)
     fused_incoming_label = Symbol(incoming_label, :l)
@@ -165,28 +188,14 @@ incoming and the outgoing MPO string with the virtual space of `A` along
         tensorexpr(:Fin, fused_incoming_label, (incoming_label, :l)),
     )
     op_e = tensorexpr(:op, (:l, :dout), (:din, :r))
-    A_e = tensorexpr(:A′, (:din, :dout), (:N, :E, :S, :W))
+    A_e = tensorexpr(:A, (:din, :dout), (:N, :E, :S, :W))
     Fout_e = tensorexpr(
         :Fout, fused_outgoing_label, (outgoing_label, :r)
     )
     rhs = Expr(:call, :*, Fin_e, op_e, A_e, Fout_e)
-    contraction = macroexpand(
+    return macroexpand(
         @__MODULE__, :(return @tensoropt $result_e := $rhs)
     )
-
-    return quote
-        _check_pepo_middle_physicalspace(A, op)
-        A′ = twistdual(A, 2)
-        Fin = _mpo_path_incoming_fuser(
-            fuser(storagetype(A), domain(A, $incoming_index), space(op, 1)'),
-            $incoming_index,
-        )
-        Fout = _mpo_path_outgoing_fuser(
-            fuser(storagetype(A), domain(A, $outgoing_index)', space(op, 4)),
-            $outgoing_index,
-        )
-        $contraction
-    end
 end
 
 """
