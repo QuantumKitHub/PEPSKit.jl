392aeec79d3954e4d8c5f4d174efdefe520b7a46
Yue Zhengyuan <yuezy1997@icloud.com>
2026-09-26 16:45:19 +0800

    Optimize NTU environment contractions using actual leg dimensions

--- src/algorithms/contractions/bondenv/benv_ntu.jl
+++ src/algorithms/contractions/bondenv/benv_ntu.jl
@@ -45,18 +45,10 @@ function bondenv_ntu(
     neighbors = [(-1, 0), (0, -1), (1, 0), (1, 1), (0, 2), (-1, 1)]
     m = collect_neighbors(state, row, col, neighbors)
     X, Y = _prepare_site_tensor(X), _prepare_site_tensor(Y)
-    @tensoropt benv[Dw1 De1; Dw0 De0] :=
-        ( # southwest half
-        cor_se(m[1, 1])[Dse1 Dse0 Ds1 Ds0] *
-            cor_sw(m[1, 0])[Dsw1 Dsw0 Ds1 Ds0] *
-            edge_w(X, hair_w(m[0, -1]))[Dnw1 Dnw0 Dw1 Dw0 Dsw1 Dsw0]
-    ) * ( # northeast half
-        cor_nw(m[-1, 0])[Dn1 Dn0 Dnw1 Dnw0] *
-            cor_ne(m[-1, 1])[Dne1 Dne0 Dn1 Dn0] *
-            edge_e(Y, hair_e(m[0, 2]))[Dne1 Dne0 Dse1 Dse0 De1 De0]
+    return _contract_ntu_NNEnv(
+        X, Y, hair_w(m[0, -1]), hair_e(m[0, 2]),
+        cor_nw(m[-1, 0]), cor_ne(m[-1, 1]), cor_sw(m[1, 0]), cor_se(m[1, 1])
     )
-    normalize!(benv, Inf)
-    return benv
 end
 
 """
@@ -109,37 +101,103 @@ function bondenv_ntu(
     se = permute(cor_se(ms[1, 2]), ((3, 4), (1, 2)))
     se1, se2 = _svd_cut!(se)
 
-    @tensoropt benv[Dw1, De1; Dw0, De0] :=
-        ( # hW
+    @tensoropt hW[DXw1 DXw0] :=
         hair_w(ms[0, -2])[Dw21 Dw20] *
-            nw1[Dnw11 Dnw10] * sw1[Dsw11 Dsw10] *
-            twistdual(ms[0, -1], 1)[phW Dnw10 DXw0 Dsw10 Dw20] *
-            conj(ms[0, -1][phW Dnw11 DXw1 Dsw11 Dw21])
-    ) * ( # hE
+        nw1[Dnw11 Dnw10] * sw1[Dsw11 Dsw10] *
+        twistdual(ms[0, -1], 1)[phW Dnw10 DXw0 Dsw10 Dw20] *
+        conj(ms[0, -1][phW Dnw11 DXw1 Dsw11 Dw21])
+    @tensoropt hE[DYe1 DYe0] :=
         hair_e(ms[0, 3])[De21 De20] *
-            ne2[Dne21 Dne20] * se2[Dse21 Dse20] *
-            twistdual(ms[0, 2], 1)[phE Dne20 De20 Dse20 DYe0] *
-            conj(ms[0, 2][phE Dne21 De21 Dse21 DYe1])
-    ) * ( # NW
+        ne2[Dne21 Dne20] * se2[Dse21 Dse20] *
+        twistdual(ms[0, 2], 1)[phE Dne20 De20 Dse20 DYe0] *
+        conj(ms[0, 2][phE Dne21 De21 Dse21 DYe1])
+    @tensoropt NW[Dn1 Dn0 DXn1 DXn0] :=
         tl[Dtl1 Dtl0] * nw2[Dnw21 Dnw20] *
-            twistdual(ms[-1, 0], 1)[pNW Dtl0 Dn0 DXn0 Dnw20] *
-            conj(ms[-1, 0][pNW Dtl1 Dn1 DXn1 Dnw21])
-    ) * ( # NE
+        twistdual(ms[-1, 0], 1)[pNW Dtl0 Dn0 DXn0 Dnw20] *
+        conj(ms[-1, 0][pNW Dtl1 Dn1 DXn1 Dnw21])
+    @tensoropt NE[DYn1 DYn0 Dn1 Dn0] :=
         tr[Dtr1 Dtr0] * ne1[Dne11 Dne10] *
-            twistdual(ms[-1, 1], 1)[pNE Dtr0 Dne10 DYn0 Dn0] *
-            conj(ms[-1, 1][pNE Dtr1 Dne11 DYn1 Dn1])
-    ) * ( # SW
+        twistdual(ms[-1, 1], 1)[pNE Dtr0 Dne10 DYn0 Dn0] *
+        conj(ms[-1, 1][pNE Dtr1 Dne11 DYn1 Dn1])
+    @tensoropt SW[DXs1 DXs0 Ds1 Ds0] :=
         bl[Dbl1 Dbl0] * sw2[Dsw21 Dsw20] *
-            twistdual(ms[1, 0], 1)[pSW DXs0 Ds0 Dbl0 Dsw20] *
-            conj(ms[1, 0][pSW DXs1 Ds1 Dbl1 Dsw21])
-    ) * ( # SE
+        twistdual(ms[1, 0], 1)[pSW DXs0 Ds0 Dbl0 Dsw20] *
+        conj(ms[1, 0][pSW DXs1 Ds1 Dbl1 Dsw21])
+    @tensoropt SE[DYs1 DYs0 Ds1 Ds0] :=
         br[Dbr1 Dbr0] * se1[Dse11 Dse10] *
-            twistdual(ms[1, 1], 1)[pSE DYs0 Dse10 Dbr0 Ds0] *
-            conj(ms[1, 1][pSE DYs1 Dse11 Dbr1 Ds1])
-    ) * conj(X[pX DXn1 Dw1 DXs1 DXw1]) *
-        twistdual(X, 1)[pX DXn0 Dw0 DXs0 DXw0] *
-        conj(Y[pY DYn1 DYe1 DYs1 De1]) *
-        twistdual(Y, 1)[pY DYn0 DYe0 DYs0 De0]
-    normalize!(benv, Inf)
-    return benv
+        twistdual(ms[1, 1], 1)[pSE DYs0 Dse10 Dbr0 Ds0] *
+        conj(ms[1, 1][pSE DYs1 Dse11 Dbr1 Ds1])
+    return _contract_ntu_NNEnv(X, Y, hW, hE, NW, NE, SW, SE)
+end
+
+# Common NN/NN+ network; each virtual bond has a bra and a ket index.
+#             NW -- NE
+#              |     |
+#       hW --- X     Y --- hE
+#              |     |
+#             SW -- SE
+# X/Y identify the central site; n/e/s/w identify its virtual leg.
+# N/S join the two upper/lower corners; pX/pY are physical indices.
+# Suffix 0 denotes ket and 1 denotes bra.
+# The four open QR indices are Xq1, Yq1, Xq0, Yq0.
+const _NTU_ENV_NETWORK = [
+    [:Xw1, :Xw0],
+    [:Ye1, :Ye0],
+    [:N1, :N0, :Xn1, :Xn0],
+    [:Yn1, :Yn0, :N1, :N0],
+    [:Xs1, :Xs0, :S1, :S0],
+    [:Ys1, :Ys0, :S1, :S0],
+    [:pX, :Xn1, :Xq1, :Xs1, :Xw1],
+    [:pX, :Xn0, :Xq0, :Xs0, :Xw0],
+    [:pY, :Yn1, :Ye1, :Ys1, :Yq1],
+    [:pY, :Yn0, :Ye0, :Ys0, :Yq0],
+]
+
+"""
+Choose the common environment's contraction order using actual leg dimensions.
+The fixed ten-tensor network is independent of the MPO path and its length.
+Floating-point costs avoid integer overflow; dense dimensions approximate symmetry-block costs.
+"""
+function _ntu_contraction_order(tensors::NamedTuple)
+    costs = Dict(
+        label => Float64(dim(space(t, axis)))
+            for (t, labels) in zip(tensors, _NTU_ENV_NETWORK)
+            for (axis, label) in enumerate(labels)
+    )
+    tree, _ = TensorOperations.optimaltree(_NTU_ENV_NETWORK, costs)
+    return first(TensorOperations.tree2indexorder(tree, _NTU_ENV_NETWORK))
+end
+
+"""
+Compile the selected order to ordinary `@tensor` contractions with statically known ranks.
+Specializations depend on the contraction order and tensor types, not on leg dimensions.
+"""
+@generated function _contract_ntu_kernel(::Val{Order}, tensors::NamedTuple) where {Order}
+    order = Expr(:tuple, Order...)
+    return macroexpand(
+        @__MODULE__, :(
+            @tensor order = $order benv[Xq1 Yq1; Xq0 Yq0] :=
+                tensors.hW[Xw1 Xw0] * tensors.hE[Ye1 Ye0] *
+                tensors.NW[N1 N0 Xn1 Xn0] * tensors.NE[Yn1 Yn0 N1 N0] *
+                tensors.SW[Xs1 Xs0 S1 S0] * tensors.SE[Ys1 Ys0 S1 S0] *
+                conj(tensors.Xbra[pX Xn1 Xq1 Xs1 Xw1]) *
+                tensors.Xket[pX Xn0 Xq0 Xs0 Xw0] *
+                conj(tensors.Ybra[pY Yn1 Ye1 Ys1 Yq1]) *
+                tensors.Yket[pY Yn0 Ye0 Ys0 Yq0]
+        )
+    )
+end
+
+"Contract an NN or NN+ boundary while keeping the central bra and ket layers separate."
+function _contract_ntu_NNEnv(X::PEPSTensor, Y::PEPSTensor, hW, hE, NW, NE, SW, SE)
+    # Bra factors are conjugated in the kernel; ket factors carry the physical-leg twist.
+    tensors = (;
+        hW, hE, NW, NE, SW, SE, Xbra = X, Xket = twistdual(X, 1),
+        Ybra = Y, Yket = twistdual(Y, 1),
+    )
+    order = _ntu_contraction_order(tensors)
+    # The runtime-selected kernel always returns the same concrete rank-(2,2) type.
+    T = tensormaptype(spacetype(X), 2, 2, TensorKit.promote_storagetype(tensors...))
+    benv = _contract_ntu_kernel(Val(Tuple(order)), tensors)::T
+    return normalize!(benv, Inf)
 end
